105924c9b565720ec3f9fde3a411e0d20ca1fba4
DingHao <dh.hiekn@gmail.com>
2020-11-16 21:13:04 +0800

    add TaskExecutor uitl

---
+++ mcn-boot-core/src/main/java/cn/hiboot/mcn/core/task/TaskExecutor.java
@@ -0,0 +1,68 @@
+package cn.hiboot.mcn.core.task;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+/**
+ * 批量异步执行器
+ *
+ * @author DingHao
+ * @since 2020/11/16 15:46
+ */
+public class TaskExecutor<T>{
+
+    private static final int DEFAULT_BATCH_SIZE = 1000;
+    private final TaskThreadPool taskThreadPool;
+    private final Iterable<T> iterable;
+    private final int perBatchSize;
+
+    public TaskExecutor(Iterable<T> iterable) {
+        this(iterable,DEFAULT_BATCH_SIZE);
+    }
+
+    public TaskExecutor(Iterable<T> iterable, int perBatchSize) {
+        this(iterable,new TaskThreadPool(Runtime.getRuntime().availableProcessors(), 10, "BatchTask"),perBatchSize);
+    }
+
+    public TaskExecutor(Iterable<T> iterable, TaskThreadPool taskThreadPool, int perBatchSize) {
+        this.iterable = iterable;
+        this.perBatchSize = perBatchSize;
+        this.taskThreadPool = taskThreadPool;
+    }
+
+    public <S> void execute(Function<T,S> convert, Consumer<List<S>> opr){
+        execute(convert,opr,true);
+    }
+
+    private  <S> void execute(Function<T,S> convert, Consumer<List<S>> opr,boolean block){
+        List<S> data = new ArrayList<>(perBatchSize);
+        for (T t : iterable) {
+            S apply = convert.apply(t);
+            if (apply == null) {
+                continue;
+            }
+            data.add(apply);
+            if(data.size() == perBatchSize){
+                write(data,opr);
+                data = new ArrayList<>();
+            }
+        }
+        if(!data.isEmpty()){
+            write(data,opr);
+        }
+        if(block){
+            taskThreadPool.closeUntilAllTaskFinish();
+        }
+    }
+
+    public <S> void nonBlockingExecute(Function<T,S> convert, Consumer<List<S>> opr){
+        execute(convert,opr,false);
+    }
+
+    private <S> void write(List<S> data,Consumer<List<S>> opr){
+        taskThreadPool.execute(() -> opr.accept(data));
+    }
+
+}
