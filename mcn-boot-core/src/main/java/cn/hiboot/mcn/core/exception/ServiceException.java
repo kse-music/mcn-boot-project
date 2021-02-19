5a730efe273f67512e6a05a353bdc39b74cc1afc
DingHao <dh.hiekn@gmail.com>
2021-02-19 20:45:51 +0800

    add ex msg

--- mcn-boot-core/src/main/java/cn/hiboot/mcn/core/exception/ServiceException.java
+++ mcn-boot-core/src/main/java/cn/hiboot/mcn/core/exception/ServiceException.java
@@ -7,12 +7,20 @@ public class ServiceException extends BaseException{
 		super(code);
 	}
 
+	private ServiceException(String msg) {
+		super(msg);
+	}
+
 	public static ServiceException newInstance(){
 		return newInstance(ExceptionKeys.SERVICE_ERROR);
 	}
 	
 	public static ServiceException newInstance(Integer code){
 		return new ServiceException(code);
+	}
+
+	public static ServiceException newInstance(String msg){
+		return new ServiceException(msg);
 	}
 
 }
