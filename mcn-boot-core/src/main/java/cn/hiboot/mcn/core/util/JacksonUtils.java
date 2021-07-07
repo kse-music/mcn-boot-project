8641a92c679a2c1ec4cb17608a59cc8e85f06e48
DingHao <dh.hiekn@gmail.com>
2021-07-07 22:02:16 +0800

    add JacksonUtils

---
+++ mcn-boot-core/src/main/java/cn/hiboot/mcn/core/util/JacksonUtils.java
@@ -0,0 +1,81 @@
+package cn.hiboot.mcn.core.util;
+
+import cn.hiboot.mcn.core.exception.JsonException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * JacksonUtils
+ *
+ * @author DingHao
+ * @since 2021/7/7 21:02
+ */
+public abstract class JacksonUtils {
+
+    private static ObjectMapper objectMapper;
+
+    public static void setObjectMapper(ObjectMapper objectMapper){
+        JacksonUtils.objectMapper = objectMapper;
+    }
+
+    public static ObjectMapper getObjectMapper() {
+        if(objectMapper == null){
+            objectMapper = new ObjectMapper();
+        }
+        return objectMapper;
+    }
+
+    public static <T> T fromJson(String content, Class<T> clazz){
+        try {
+            return getObjectMapper().readValue(content,clazz);
+        } catch (JsonProcessingException e) {
+            throw JsonException.newInstance(e);
+        }
+    }
+
+    public static <T> List<T> fromList(String content,Class<T> clazz){
+        try {
+            return getObjectMapper().readValue(content,getObjectMapper().getTypeFactory().constructCollectionType(List.class, clazz));
+        } catch (JsonProcessingException e) {
+            throw JsonException.newInstance(e);
+        }
+    }
+
+    public static <K, V> List<Map<K,V>> fromListMap(String content,Class<K> keyClass,Class<V> valueClass){
+        try {
+            return getObjectMapper().readValue(content,getObjectMapper().getTypeFactory().constructCollectionType(List.class,
+                    getObjectMapper().getTypeFactory().constructMapType(Map.class,keyClass,valueClass)));
+        } catch (JsonProcessingException e) {
+            throw JsonException.newInstance(e);
+        }
+    }
+
+    public static Map<String, Object> fromMap(String content){
+        try {
+            return getObjectMapper().readValue(content,new TypeReference<Map<String, Object>>(){});
+        } catch (JsonProcessingException e) {
+            throw JsonException.newInstance(e);
+        }
+    }
+
+    public static <K,V> Map<K, V> fromMap(String content,Class<K> keyClass,Class<V> valueClass){
+        try {
+            return getObjectMapper().readValue(content,getObjectMapper().getTypeFactory().constructMapType(Map.class,keyClass,valueClass));
+        } catch (JsonProcessingException e) {
+            throw JsonException.newInstance(e);
+        }
+    }
+
+    public static String toJson(Object value){
+        try {
+            return getObjectMapper().writeValueAsString(value);
+        } catch (JsonProcessingException e) {
+            throw JsonException.newInstance(e);
+        }
+    }
+
+}
