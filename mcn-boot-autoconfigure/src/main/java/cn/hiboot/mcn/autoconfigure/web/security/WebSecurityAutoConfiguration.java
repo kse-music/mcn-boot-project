7b3011d9a9ac6aa4effa726dc66fa8d6aaf4e48d
DingHao <dh.hiekn@gmail.com>
2021-07-11 19:08:08 +0800

    use WebSecurityCustomizer
    (cherry picked from commit b050fb5a8cc117534e8c9e0ef20b178084aec824)

--- mcn-boot-autoconfigure/src/main/java/cn/hiboot/mcn/autoconfigure/web/security/WebSecurityAutoConfiguration.java
+++ mcn-boot-autoconfigure/src/main/java/cn/hiboot/mcn/autoconfigure/web/security/WebSecurityAutoConfiguration.java
@@ -1,60 +1,47 @@
 package cn.hiboot.mcn.autoconfigure.web.security;
 
-import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 
 /**
  * WebSecurityAutoConfiguration
+ * 配置忽略的请求路径
  *
  * @author DingHao
  * @since 2021/5/23 23:36
  */
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnClass(WebSecurityConfigurerAdapter.class)
-@ConditionalOnMissingBean(WebSecurityConfigurerAdapter.class)
+@ConditionalOnBean(WebSecurityConfiguration.class)
 @EnableConfigurationProperties(WebSecurityProperties.class)
 @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
-public class WebSecurityAutoConfiguration extends WebSecurityConfigurerAdapter {
+@AutoConfigureAfter(SecurityAutoConfiguration.class)
+public class WebSecurityAutoConfiguration implements WebSecurityCustomizer {
 
     private static final String[] IGNORE_PATH = {"/v2/api-docs", "/swagger-resources/**","/doc.html", "/webjars/**"};
 
     private final WebSecurityProperties webSecurityProperties;
-    private final ObjectProvider<HttpSecurityConfigCustomizer> httpCustomizers;
-    private final ObjectProvider<WebSecurityConfigCustomizer> webCustomizers;
 
-    public WebSecurityAutoConfiguration(WebSecurityProperties webSecurityProperties,
-                                        ObjectProvider<HttpSecurityConfigCustomizer> httpCustomizers,
-                                        ObjectProvider<WebSecurityConfigCustomizer> webCustomizers){
+    public WebSecurityAutoConfiguration(WebSecurityProperties webSecurityProperties){
         this.webSecurityProperties = webSecurityProperties;
-        this.httpCustomizers = httpCustomizers;
-        this.webCustomizers = webCustomizers;
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        for (HttpSecurityConfigCustomizer httpCustomizer : httpCustomizers) {
-            httpCustomizer.customize(http);
-        }
-    }
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
+    public void customize(WebSecurity web) {
         if(webSecurityProperties.isEnableDefaultIgnore()){
             web.ignoring().antMatchers(IGNORE_PATH);
         }
         if(webSecurityProperties.getExcludes() != null){
             web.ignoring().antMatchers(webSecurityProperties.getExcludes());
         }
-        for (WebSecurityConfigCustomizer webCustomizer : webCustomizers) {
-            webCustomizer.customize(web);
-        }
     }
-
 }
