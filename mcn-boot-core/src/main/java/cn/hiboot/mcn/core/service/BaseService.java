a795b4744485efbf52ffe187051a642a85d916f9
DingHao <dh.hiekn@gmail.com>
2021-07-20 14:36:35 +0800

    use default method remove BaseServiceImpl.java
    (cherry picked from commit b53aafe4fdf686e4d9aeca825ab941b197f6205e)

--- mcn-boot-core/src/main/java/cn/hiboot/mcn/core/service/BaseService.java
+++ mcn-boot-core/src/main/java/cn/hiboot/mcn/core/service/BaseService.java
@@ -1,21 +1,55 @@
 package cn.hiboot.mcn.core.service;
 
 
+import cn.hiboot.mcn.core.mapper.BaseMapper;
 import cn.hiboot.mcn.core.model.result.RestResp;
 
 import java.util.List;
 
 public interface BaseService<T,PK> {
 
-    void assertSelfData(PK id);
-    T save(T pojo);
-    T saveSelective(T pojo);
-    void deleteByPrimaryKey(PK id);
-    T getByPrimaryKey(PK id);
-    void updateByPrimaryKeySelective(T pojo);
-    RestResp<List<T>> listPage(T pojo);
-    List<T> pageSelect(T pojo);
-    int pageCount(T pojo);
-    List<T> selectByCondition(T pojo);
+    BaseMapper<T,PK> getBaseMapper();
+
+    default void assertSelfData(PK id){
+
+    }
+
+    default T save(T pojo){
+        getBaseMapper().insert(pojo);
+        return pojo;
+    }
+
+    default T saveSelective(T pojo){
+        getBaseMapper().insertSelective(pojo);
+        return pojo;
+    }
+
+    default void deleteByPrimaryKey(PK id){
+        getBaseMapper().deleteByPrimaryKey(id);
+    }
+
+    default T getByPrimaryKey(PK id){
+        return getBaseMapper().selectByPrimaryKey(id);
+    }
+
+    default void updateByPrimaryKeySelective(T pojo){
+        getBaseMapper().updateByPrimaryKeySelective(pojo);
+    }
+
+    default RestResp<List<T>> listPage(T pojo){
+        return new RestResp<>(pageSelect(pojo),pageCount(pojo));
+    }
+
+    default List<T> pageSelect(T pojo){
+        return getBaseMapper().pageSelect(pojo);
+    }
+
+    default int pageCount(T pojo){
+        return getBaseMapper().pageCount(pojo);
+    }
+
+    default List<T> selectByCondition(T pojo){
+        return getBaseMapper().selectByCondition(pojo);
+    }
 
 }
