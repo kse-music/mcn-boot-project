c01f4d86faab2ab166bfc685e40409044c1325f8
DingHao <dh.hiekn@gmail.com>
2021-07-27 22:42:50 +0800

    add ValidatorAutoConfiguration
    (cherry picked from commit 01092d1a7652817ae234307f0b70893c99ebe6ab)

--- mcn-boot-autoconfigure/src/main/java/cn/hiboot/mcn/autoconfigure/context/McnBeanPostProcessor.java
+++ mcn-boot-autoconfigure/src/main/java/cn/hiboot/mcn/autoconfigure/context/McnBeanPostProcessor.java
@@ -1,12 +1,9 @@
 package cn.hiboot.mcn.autoconfigure.context;
 
-import cn.hiboot.mcn.autoconfigure.web.validator.LocalValidatorCustomizer;
 import cn.hiboot.mcn.core.util.JacksonUtils;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.BeansException;
-import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.config.BeanPostProcessor;
-import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 /**
  *
@@ -16,26 +13,6 @@ import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
  * @since 2019/1/7 2:09
  */
 public class McnBeanPostProcessor implements BeanPostProcessor {
-
-    private final BeanFactory beanFactory;
-
-    public McnBeanPostProcessor(BeanFactory beanFactory) {
-        this.beanFactory = beanFactory;
-    }
-
-    @Override
-    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
-        if(bean instanceof LocalValidatorFactoryBean){
-            LocalValidatorFactoryBean b = (LocalValidatorFactoryBean) bean;
-            if(!b.getValidationPropertyMap().containsKey("hibernate.validator.fail_fast")){
-                b.getValidationPropertyMap().put("hibernate.validator.fail_fast", "true");//default use fast fail
-            }
-            for (LocalValidatorCustomizer localValidatorCustomizer : beanFactory.getBeanProvider(LocalValidatorCustomizer.class)) {
-                localValidatorCustomizer.customize(b);
-            }
-        }
-        return bean;
-    }
 
     @Override
     public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
