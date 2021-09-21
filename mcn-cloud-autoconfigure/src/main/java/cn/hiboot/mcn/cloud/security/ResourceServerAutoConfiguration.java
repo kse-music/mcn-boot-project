55e95aa8232722cd5ecf7784b5aa15c4c9227108
DingHao <dh.hiekn@gmail.com>
2021-09-21 15:55:12 +0800

    ResourceServerAutoConfiguration auto before ManagementWebSecurityAutoConfiguration

--- mcn-cloud-autoconfigure/src/main/java/cn/hiboot/mcn/cloud/security/ResourceServerAutoConfiguration.java
+++ mcn-cloud-autoconfigure/src/main/java/cn/hiboot/mcn/cloud/security/ResourceServerAutoConfiguration.java
@@ -1,7 +1,9 @@
 package cn.hiboot.mcn.cloud.security;
 
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,8 +18,10 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtAut
  * @since 2021/8/20 10:22
  */
 @Configuration(proxyBeanMethods = false)
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
 @ConditionalOnClass({DefaultAuthenticationEventPublisher.class, JwtAuthenticationToken.class})
 @ConditionalOnProperty(prefix = "mcn.resource.server",name = "enabled",havingValue = "true")
+@AutoConfigureBefore(name = "org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration")
 public class ResourceServerAutoConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
