652e6fc320da61b4834aca1fc8bcb46c5e99767d
DingHao <dh.hiekn@gmail.com>
2021-09-24 11:40:21 +0800

    优化WebSecurityAutoConfiguration配置条件

--- mcn-boot-autoconfigure/src/main/java/cn/hiboot/mcn/autoconfigure/web/security/WebSecurityAutoConfiguration.java
+++ mcn-boot-autoconfigure/src/main/java/cn/hiboot/mcn/autoconfigure/web/security/WebSecurityAutoConfiguration.java
@@ -1,14 +1,10 @@
 package cn.hiboot.mcn.autoconfigure.web.security;
 
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 
@@ -21,10 +17,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  */
 @Configuration(proxyBeanMethods = false)
 @ConditionalOnClass(WebSecurityConfigurerAdapter.class)
-@ConditionalOnBean(WebSecurityConfiguration.class)
 @EnableConfigurationProperties(WebSecurityProperties.class)
 @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
-@AutoConfigureAfter(SecurityAutoConfiguration.class)
 public class WebSecurityAutoConfiguration implements WebSecurityCustomizer {
 
     private final WebSecurityProperties webSecurityProperties;
