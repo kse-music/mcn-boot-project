d14f41a2bca4d7c60b035618d4b5e299909decc7
DingHao <dh.hiekn@gmail.com>
2021-10-19 16:55:04 +0800

    新增OAuth2ResourceServerConfigurerCustomizer

--- mcn-cloud-autoconfigure/src/main/java/cn/hiboot/mcn/cloud/security/ResourceServerAutoConfiguration.java
+++ mcn-cloud-autoconfigure/src/main/java/cn/hiboot/mcn/cloud/security/ResourceServerAutoConfiguration.java
@@ -1,10 +1,12 @@
 package cn.hiboot.mcn.cloud.security;
 
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.autoconfigure.security.ConditionalOnDefaultWebSecurity;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
+import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
@@ -23,11 +25,23 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtAut
 @ConditionalOnDefaultWebSecurity
 public class ResourceServerAutoConfiguration extends WebSecurityConfigurerAdapter {
 
+    private final ObjectProvider<OAuth2ResourceServerConfigurerCustomizer> customizers;
+
+    public ResourceServerAutoConfiguration(ObjectProvider<OAuth2ResourceServerConfigurerCustomizer> customizers) {
+        this.customizers = customizers;
+    }
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests().anyRequest().authenticated()
-                .and().oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+                .and().oauth2ResourceServer(o -> applyCustom(o.jwt().and()));
+    }
+
+    private <H extends HttpSecurityBuilder<H>> void applyCustom(OAuth2ResourceServerConfigurer<H> configurer){
+        for (OAuth2ResourceServerConfigurerCustomizer customizer : customizers) {
+            customizer.customize(configurer);
+        }
     }
 
 }
