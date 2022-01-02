5074f6aeccdea2908c8770198ad123402e7b5252
DingHao <dh.hiekn@gmail.com>
2022-01-02 22:45:12 +0800

    remove CorsConfigurationSource.class condition

--- mcn-boot-autoconfigure/src/main/java/cn/hiboot/mcn/autoconfigure/web/filter/FilterAutoConfiguration.java
+++ mcn-boot-autoconfigure/src/main/java/cn/hiboot/mcn/autoconfigure/web/filter/FilterAutoConfiguration.java
@@ -9,7 +9,6 @@ import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
@@ -31,7 +30,6 @@ public class FilterAutoConfiguration {
     }
 
     @Bean
-    @ConditionalOnClass(CorsConfigurationSource.class)
     @ConditionalOnProperty(prefix = "filter", name = "cross", havingValue = "true")
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
