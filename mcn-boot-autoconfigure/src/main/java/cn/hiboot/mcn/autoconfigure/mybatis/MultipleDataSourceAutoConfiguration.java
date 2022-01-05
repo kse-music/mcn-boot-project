987bbfc44507162d6b53c70e192461186f641877
DingHao <dh.hiekn@gmail.com>
2022-01-05 18:30:48 +0800

    can't both support single and multiple data source configuration

--- mcn-boot-autoconfigure/src/main/java/cn/hiboot/mcn/autoconfigure/mybatis/MultipleDataSourceAutoConfiguration.java
+++ mcn-boot-autoconfigure/src/main/java/cn/hiboot/mcn/autoconfigure/mybatis/MultipleDataSourceAutoConfiguration.java
@@ -3,47 +3,60 @@ package cn.hiboot.mcn.autoconfigure.mybatis;
 import cn.hiboot.mcn.core.config.McnConstant;
 import com.zaxxer.hikari.HikariDataSource;
 import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.boot.autoconfigure.SpringBootVFS;
 import org.mybatis.spring.mapper.ClassPathMapperScanner;
 import org.springframework.beans.factory.config.BeanDefinition;
 import org.springframework.beans.factory.config.RuntimeBeanReference;
 import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.boot.autoconfigure.condition.ConditionOutcome;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.SpringBootCondition;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.bind.Bindable;
 import org.springframework.boot.context.properties.bind.Binder;
 import org.springframework.context.EnvironmentAware;
 import org.springframework.context.ResourceLoaderAware;
-import org.springframework.context.annotation.Conditional;
-import org.springframework.context.annotation.FullyQualifiedAnnotationBeanNameGenerator;
-import org.springframework.context.annotation.ImportBeanDefinitionRegistrar;
+import org.springframework.context.annotation.*;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 import org.springframework.core.type.AnnotationMetadata;
 
+import java.io.IOException;
+
 /**
- * MultipleDataSourceBeanFactoryPostProcessor
+ * MultipleDataSourceAutoConfiguration
  *
  * @author DingHao
  * @since 2022/1/2 22:21
  */
+@Configuration(proxyBeanMethods = false)
 @ConditionalOnClass(HikariDataSource.class)
-@Conditional(MultipleDataSourceCondition.class)
-public class MultipleDataSourceConfiguration implements ImportBeanDefinitionRegistrar, EnvironmentAware, ResourceLoaderAware {
+@Conditional(MultipleDataSourceAutoConfiguration.MultipleDataSourceCondition.class)
+public class MultipleDataSourceAutoConfiguration implements ImportBeanDefinitionRegistrar, EnvironmentAware, ResourceLoaderAware {
+
+    private static final String MULTIPLY_DATASOURCE_CONFIG_KEY = "multiply.datasource.name";
 
     private ResourceLoader resourceLoader;
     private Environment environment;
 
     @Override
     public void registerBeanDefinitions(AnnotationMetadata importingClassMetadata, BeanDefinitionRegistry registry){
-        String[] dbs = environment.getProperty(MybatisQuickAutoConfiguration.MULTIPLY_DATASOURCE_CONFIG_KEY, String[].class,new String[0]);
+        String[] dbs = environment.getProperty(MULTIPLY_DATASOURCE_CONFIG_KEY, String[].class,new String[0]);
         String basePackage = environment.getProperty(McnConstant.APP_BASE_PACKAGE);
+        ResourcePatternResolver pathResolver = new PathMatchingResourcePatternResolver();
+
         for (String dsName : dbs) {
             String sqlSessionFactoryName = dsName + "SqlSessionFactory";
             scanMapper(registry,sqlSessionFactoryName,basePackage + ".dao." + dsName);
+
+            registry.registerBeanDefinition(dsName + "sqlSessionTemplate", BeanDefinitionBuilder.genericBeanDefinition(SqlSessionTemplate.class)
+                    .setRole(BeanDefinition.ROLE_INFRASTRUCTURE).setSynthetic(true).addConstructorArgReference(sqlSessionFactoryName).getBeanDefinition());
 
             String dataSourceName = dsName + "DataSource";
             registry.registerBeanDefinition(dataSourceName, BeanDefinitionBuilder.genericBeanDefinition(HikariDataSource.class,() -> createDataSource(dsName))
@@ -51,23 +64,31 @@ public class MultipleDataSourceConfiguration implements ImportBeanDefinitionRegi
 
             SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
             factoryBean.setVfs(SpringBootVFS.class);
-            ResourcePatternResolver pathResolver = new PathMatchingResourcePatternResolver();
             org.apache.ibatis.session.Configuration conf = new org.apache.ibatis.session.Configuration();
             conf.setMapUnderscoreToCamelCase(true);
-            try {
-                registry.registerBeanDefinition(sqlSessionFactoryName,
-                        BeanDefinitionBuilder.rootBeanDefinition(SqlSessionFactoryBean.class).setRole(BeanDefinition.ROLE_INFRASTRUCTURE)
-                                .addPropertyValue("vfs", SpringBootVFS.class)
-                                .addPropertyValue("dataSource", new RuntimeBeanReference(dataSourceName))
-                                .addPropertyValue("mapperLocations", pathResolver.getResources("classpath:mapper/"+dsName+"/*.xml"))
-                                .addPropertyValue("typeAliasesPackage", basePackage + ".bean")
-                                .addPropertyValue("typeHandlersPackage", basePackage + ".dao.handler")
-                                .addPropertyValue("configuration", conf)
-                                .getBeanDefinition());
-            } catch (Exception e) {
-                //
-            }
+            registry.registerBeanDefinition(sqlSessionFactoryName,
+                    loadMapper(pathResolver, dsName)
+                            .addPropertyValue("dataSource", new RuntimeBeanReference(dataSourceName))
+                            .addPropertyValue("typeAliasesPackage", basePackage + ".bean" + dsName)
+                            .addPropertyValue("typeHandlersPackage", basePackage + ".dao.handler" + dsName)
+                            .addPropertyValue("configuration", conf).getBeanDefinition());
+
         }
+    }
+
+    private BeanDefinitionBuilder loadMapper(ResourcePatternResolver pathResolver, String dsName){
+        BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder.rootBeanDefinition(SqlSessionFactoryBean.class)
+                .setRole(BeanDefinition.ROLE_INFRASTRUCTURE).addPropertyValue("vfs", SpringBootVFS.class);
+        Resource[] resources = null;
+        try {
+            resources = pathResolver.getResources("classpath:mapper/" + dsName + "/*.xml");
+        } catch (IOException e) {
+            //ignore not exist
+        }
+        if(resources != null){
+            beanDefinitionBuilder.addPropertyValue("mapperLocations", resources);
+        }
+        return beanDefinitionBuilder;
     }
 
     private void scanMapper(BeanDefinitionRegistry registry, String sqlSessionFactoryName, String pkg){
@@ -94,5 +115,16 @@ public class MultipleDataSourceConfiguration implements ImportBeanDefinitionRegi
     @Override
     public void setResourceLoader(ResourceLoader resourceLoader) {
         this.resourceLoader = resourceLoader;
+    }
+
+    protected static class MultipleDataSourceCondition extends SpringBootCondition {
+        @Override
+        public ConditionOutcome getMatchOutcome(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            String[] dbs = context.getEnvironment().getProperty(MULTIPLY_DATASOURCE_CONFIG_KEY, String[].class);
+            if(dbs != null && dbs.length > 0){
+                return ConditionOutcome.match("match multiple datasource");
+            }
+            return ConditionOutcome.noMatch("no multiple datasource");
+        }
     }
 }
---
+++ mcn-boot-autoconfigure/src/main/java/cn/hiboot/mcn/autoconfigure/mybatis/SingleDataSourceAutoConfiguration.java
@@ -0,0 +1,61 @@
+package cn.hiboot.mcn.autoconfigure.mybatis;
+
+import cn.hiboot.mcn.core.config.McnConstant;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.mapper.MapperScannerConfigurer;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.BeanDefinitionRegistryPostProcessor;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.context.EnvironmentAware;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.env.Environment;
+import org.springframework.util.StringUtils;
+
+/**
+ * MybatisQuickAutoConfiguration
+ *
+ * @author DingHao
+ * @since 2022/1/2 0:25
+ */
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnClass({ SqlSessionFactory.class, SqlSessionFactoryBean.class })
+public class SingleDataSourceAutoConfiguration implements BeanDefinitionRegistryPostProcessor, EnvironmentAware, Ordered {
+
+    private Environment environment;
+
+    @Override
+    public void postProcessBeanDefinitionRegistry(BeanDefinitionRegistry registry) throws BeansException {
+        String name = MapperScannerConfigurer.class.getName();//use default config
+        if(registry.containsBeanDefinition(name)){
+            BeanDefinition beanDefinition = registry.getBeanDefinition(name);
+            beanDefinition.getPropertyValues().removePropertyValue("annotationClass");
+            String toScanPkg = environment.getProperty(McnConstant.APP_BASE_PACKAGE) + ".dao";
+            String additionalPkg = environment.getProperty("mapper.scan.additional-package", "");
+            if (StringUtils.hasText(additionalPkg)) {
+                toScanPkg += "," + additionalPkg;
+            }
+            beanDefinition.getPropertyValues().addPropertyValue("basePackage", toScanPkg);
+        }
+    }
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+    }
+
+    @Override
+    public void setEnvironment(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Override
+    public int getOrder() {
+            return 0;
+        }
+
+
+}
