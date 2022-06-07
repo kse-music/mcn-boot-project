98d0c01b0ffe0b9a4cf1242085de21160fc27c59
DingHao <dh.hiekn@gmail.com>
2022-06-07 17:39:13 +0800

    2.3.12.1

--- mcn-boot-autoconfigure/src/main/java/cn/hiboot/mcn/autoconfigure/web/security/WebSecurityAutoConfiguration.java
+++ mcn-boot-autoconfigure/src/main/java/cn/hiboot/mcn/autoconfigure/web/security/WebSecurityAutoConfiguration.java
@@ -3,16 +3,17 @@ package cn.hiboot.mcn.autoconfigure.web.security;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.OrRequestMatcher;
 import org.springframework.security.web.util.matcher.RequestMatcher;
 
-import javax.servlet.Filter;
+import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -38,22 +39,34 @@ public class WebSecurityAutoConfiguration {
     }
 
     @Bean
-    @Order(-10000)
-    public Filter webSecurityCustomizer(){
-        List<RequestMatcher> requestMatchers = new ArrayList<>();
-        if(webSecurityProperties.isEnableDefaultIgnore()){
-            requestMatchers.addAll(Arrays.stream(webSecurityProperties.getDefaultExcludeUrls()).map(AntPathRequestMatcher::new).collect(Collectors.toList()));
+    public FilterRegistrationBean<IgnoreUrlFilter> corsFilterRegistration() {
+        FilterRegistrationBean<IgnoreUrlFilter> filterRegistrationBean = new FilterRegistrationBean<>(new IgnoreUrlFilter(webSecurityProperties));
+        filterRegistrationBean.setOrder(-1000);
+        return filterRegistrationBean;
+    }
+
+    protected static class IgnoreUrlFilter implements Filter{
+
+        private final RequestMatcher requestMatcher;
+
+        public IgnoreUrlFilter(WebSecurityProperties webSecurityProperties){
+            List<RequestMatcher> requestMatchers = new ArrayList<>();
+            if(webSecurityProperties.isEnableDefaultIgnore()){
+                requestMatchers.addAll(Arrays.stream(webSecurityProperties.getDefaultExcludeUrls()).map(AntPathRequestMatcher::new).collect(Collectors.toList()));
+            }
+            if(webSecurityProperties.getExcludeUrls() != null){
+                requestMatchers.addAll(Arrays.stream(webSecurityProperties.getExcludeUrls()).map(AntPathRequestMatcher::new).collect(Collectors.toList()));
+            }
+            this.requestMatcher = new OrRequestMatcher(requestMatchers);
         }
-        if(webSecurityProperties.getExcludeUrls() != null){
-            requestMatchers.addAll(Arrays.stream(webSecurityProperties.getExcludeUrls()).map(AntPathRequestMatcher::new).collect(Collectors.toList()));
-        }
-       RequestMatcher requestMatcher = new OrRequestMatcher(requestMatchers);
-        return (request, response,chain) -> {
+
+        @Override
+        public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
             if(requestMatcher.matches((HttpServletRequest) request)){
                 return;
             }
             chain.doFilter(request,response);
-        };
-    }
+        }
 
+    }
 }
