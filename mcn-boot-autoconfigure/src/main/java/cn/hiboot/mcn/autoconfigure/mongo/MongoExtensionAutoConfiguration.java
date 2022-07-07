c999d162427a720be977590d4ad0122c418a76f6
DingHao <dh.hiekn@gmail.com>
2022-07-07 10:19:45 +0800

    use obj model config MongoClient
    (cherry picked from commit 092ba9159e7437cd2c84b415482c3fc2717c5b7f)

---
+++ mcn-boot-autoconfigure/src/main/java/cn/hiboot/mcn/autoconfigure/mongo/MongoExtensionAutoConfiguration.java
@@ -0,0 +1,71 @@
+package cn.hiboot.mcn.autoconfigure.mongo;
+
+import com.mongodb.ReadConcern;
+import com.mongodb.ReadPreference;
+import com.mongodb.WriteConcern;
+import com.mongodb.client.MongoClient;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.mongo.MongoClientSettingsBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * MongoExtensionAutoConfiguration
+ *
+ * @author DingHao
+ * @since 2022/7/5 13:08
+ */
+@Configuration(proxyBeanMethods = false)
+@EnableConfigurationProperties(MongoExtensionProperties.class)
+@ConditionalOnClass(MongoClient.class)
+@ConditionalOnMissingBean(type = "org.springframework.data.mongodb.MongoDatabaseFactory")
+public class MongoExtensionAutoConfiguration {
+
+    private static final Map<String, ReadConcern> NAMED_CONCERNS;
+
+    static {
+        NAMED_CONCERNS = new HashMap<>();
+        for (final Field f : ReadConcern.class.getFields()) {
+            if (Modifier.isStatic(f.getModifiers()) && f.getType().equals(ReadConcern.class)) {
+                String key = f.getName().toLowerCase();
+                try {
+                    NAMED_CONCERNS.put(key, (ReadConcern) f.get(null));
+                } catch (IllegalAccessException e) {
+                    //ignore;
+                }
+            }
+        }
+    }
+
+    @Bean
+    MongoClientSettingsBuilderCustomizer defaultMongoClientSettingsBuilderCustomizer(MongoExtensionProperties mongo){
+        return builder -> {
+
+            builder.applyToConnectionPoolSettings(connectionPool -> {
+                connectionPool.maxSize(mongo.getPool().getMaxSize());
+                connectionPool.minSize(mongo.getPool().getMinSize());
+                connectionPool.maxWaitTime(mongo.getPool().getMaxWaitTime(),TimeUnit.MILLISECONDS);
+            });
+
+            builder.readPreference(ReadPreference.valueOf(mongo.getReadPreference().name()));
+            builder.writeConcern(WriteConcern.valueOf(mongo.getWriteConcern().name()));
+            builder.readConcern(Objects.isNull(mongo.getReadConcern()) ? ReadConcern.DEFAULT : NAMED_CONCERNS.get(mongo.getReadConcern().name()));
+
+            builder.applyToSocketSettings(socket -> {
+                socket.connectTimeout(mongo.getSocket().getConnectTimeout(),TimeUnit.MILLISECONDS);
+                socket.readTimeout(mongo.getSocket().getReadTimeout(),TimeUnit.MILLISECONDS);
+            });
+
+        };
+    }
+
+}
---
+++ mcn-boot-autoconfigure/src/main/java/cn/hiboot/mcn/autoconfigure/mongo/MongoExtensionProperties.java
@@ -0,0 +1,162 @@
+package cn.hiboot.mcn.autoconfigure.mongo;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * MongoExtensionProperties
+ *
+ * @author DingHao
+ * @since 2022/7/5 13:08
+ */
+@ConfigurationProperties("mongo")
+public class MongoExtensionProperties {
+
+    /**
+     * 读取偏好设置
+     */
+    private ReadPreference readPreference = ReadPreference.primary;
+
+    private WriteConcern writeConcern = WriteConcern.acknowledged;
+
+    private ReadConcern readConcern;
+
+    private Pool pool = new Pool();
+
+    private Socket socket = new Socket();
+
+    public ReadPreference getReadPreference() {
+        return readPreference;
+    }
+
+    public void setReadPreference(ReadPreference readPreference) {
+        this.readPreference = readPreference;
+    }
+
+    public WriteConcern getWriteConcern() {
+        return writeConcern;
+    }
+
+    public void setWriteConcern(WriteConcern writeConcern) {
+        this.writeConcern = writeConcern;
+    }
+
+    public ReadConcern getReadConcern() {
+        return readConcern;
+    }
+
+    public void setReadConcern(ReadConcern readConcern) {
+        this.readConcern = readConcern;
+    }
+
+    public Pool getPool() {
+        return pool;
+    }
+
+    public void setPool(Pool pool) {
+        this.pool = pool;
+    }
+
+    public Socket getSocket() {
+        return socket;
+    }
+
+    public void setSocket(Socket socket) {
+        this.socket = socket;
+    }
+
+    public enum ReadPreference {
+        primary,
+        secondary,
+        secondaryPreferred,
+        primaryPreferred,
+        nearest
+    }
+
+    public enum WriteConcern{
+        acknowledged,
+        w1,
+        w2,
+        w3,
+        unacknowledged,
+        journaled,
+        majority
+    }
+
+    public enum ReadConcern{
+        local,
+        majority,
+        linearizable,
+        snapshot,
+        available
+    }
+
+    public static class Pool{
+        /**
+         * 线程池允许的最大连接数,空闲时保存在池中
+         */
+        private int maxSize = 100;
+
+        /**
+         * 空闲时保存在池中最小连接数
+         */
+        private int minSize = 0;
+
+        /**
+         * 等待一个可用连接最大等待时间,单位ms
+         */
+        private long maxWaitTime = 120000;
+
+        public int getMaxSize() {
+            return maxSize;
+        }
+
+        public void setMaxSize(int maxSize) {
+            this.maxSize = maxSize;
+        }
+
+        public int getMinSize() {
+            return minSize;
+        }
+
+        public void setMinSize(int minSize) {
+            this.minSize = minSize;
+        }
+
+        public long getMaxWaitTime() {
+            return maxWaitTime;
+        }
+
+        public void setMaxWaitTime(long maxWaitTime) {
+            this.maxWaitTime = maxWaitTime;
+        }
+    }
+
+    public static class Socket{
+
+        /**
+         * socket连接超时时间,单位ms
+         */
+        private int connectTimeout = 10000;
+
+        /**
+         * socket读取超时时间,单位ms
+         */
+        private int readTimeout;
+
+        public int getConnectTimeout() {
+            return connectTimeout;
+        }
+
+        public void setConnectTimeout(int connectTimeout) {
+            this.connectTimeout = connectTimeout;
+        }
+
+        public int getReadTimeout() {
+            return readTimeout;
+        }
+
+        public void setReadTimeout(int readTimeout) {
+            this.readTimeout = readTimeout;
+        }
+    }
+}
