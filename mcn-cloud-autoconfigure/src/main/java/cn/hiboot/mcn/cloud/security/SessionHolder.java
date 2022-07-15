c8c7e9bb11b1e3481395bd45787103b58318194c
DingHao <dh.hiekn@gmail.com>
2022-07-15 18:48:00 +0800

    rich SessionHolder
    (cherry picked from commit 5d8e8441c7997e479732c0637926e85b48a01835)

--- mcn-cloud-autoconfigure/src/main/java/cn/hiboot/mcn/cloud/security/SessionHolder.java
+++ mcn-cloud-autoconfigure/src/main/java/cn/hiboot/mcn/cloud/security/SessionHolder.java
@@ -4,6 +4,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 
+import java.util.Collections;
 import java.util.Map;
 
 /**
@@ -17,28 +18,23 @@ public interface SessionHolder {
     String IDENTIFY = "userId";
 
     static String getUserId() {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if(authentication != null){
-            if(authentication instanceof JwtAuthenticationToken){
-                JwtAuthenticationToken jwtAuthenticationToken = (JwtAuthenticationToken) authentication;
-                Map<String, Object> tokenAttributes = jwtAuthenticationToken.getTokenAttributes();
-                Map<String,Object> user = (Map<String,Object>)tokenAttributes.get("user_name");
-                return user.get(IDENTIFY).toString();
-            }
-        }
-        return null;
+        JwtAuthenticationToken jwtAuthenticationToken = getJwtAuthenticationToken();
+        return jwtAuthenticationToken == null ? null : jwtAuthenticationToken.getToken().getClaimAsMap("user_name").get(IDENTIFY).toString();
     }
 
+    static Map<String, Object> getClaims() {
+        JwtAuthenticationToken jwtAuthenticationToken = getJwtAuthenticationToken();
+        return jwtAuthenticationToken == null ? Collections.emptyMap() : jwtAuthenticationToken.getToken().getClaims();
+    }
 
     static String getToken() {
+        JwtAuthenticationToken jwtAuthenticationToken = getJwtAuthenticationToken();
+        return jwtAuthenticationToken == null ? null : jwtAuthenticationToken.getToken().getTokenValue();
+    }
+
+    static JwtAuthenticationToken getJwtAuthenticationToken() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        if(authentication != null){
-            if(authentication instanceof JwtAuthenticationToken){
-                JwtAuthenticationToken jwtAuthenticationToken = (JwtAuthenticationToken) authentication;
-                return jwtAuthenticationToken.getToken().getTokenValue();
-            }
-        }
-        return null;
+        return authentication instanceof JwtAuthenticationToken ? (JwtAuthenticationToken) authentication : null;
     }
 
 }
