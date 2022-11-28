e749eeffef0002b19a3e647ed8968cabe7d822a2
DingHao <dh.hiekn@gmail.com>
2022-11-28 14:52:36 +0800

    rename method name
    (cherry picked from commit dd5350fe47039af11bea1f0841d422456f8457c1)

--- mcn-cloud-autoconfigure/src/main/java/cn/hiboot/mcn/cloud/security/SessionHolder.java
+++ mcn-cloud-autoconfigure/src/main/java/cn/hiboot/mcn/cloud/security/SessionHolder.java
@@ -15,21 +15,22 @@ import java.util.Map;
  * @since 2021/8/20 10:32
  */
 public interface SessionHolder {
+    String BEARER_PREFIX = "Bearer ";
 
     static String getUserId() {
-        return getUserId("userId");
+        return get("userId");
     }
 
-    static String getUserId(String userIdKey) {
-        Object userId = getUserName().get(userIdKey);
+    static String get(String userIdKey) {
+        Object userId = getUser().get(userIdKey);
         return userId == null ? null : userId.toString();
     }
 
-    static Map<String, Object> getUserName() {
-        return getUserName("user_name");
+    static Map<String, Object> getUser() {
+        return getUser("user_name");
     }
 
-    static Map<String, Object> getUserName(String userNameKey) {
+    static Map<String, Object> getUser(String userNameKey) {
         Jwt jwt = getJwtToken();
         return jwt == null ? Collections.emptyMap() : jwt.getClaimAsMap(userNameKey);
     }
@@ -41,7 +42,7 @@ public interface SessionHolder {
 
     static String getBearerToken() {
         String token = getToken();
-        return token == null ? null : "Bearer ".concat(token);
+        return token == null ? null : BEARER_PREFIX.concat(token);
     }
 
     static String getToken() {
