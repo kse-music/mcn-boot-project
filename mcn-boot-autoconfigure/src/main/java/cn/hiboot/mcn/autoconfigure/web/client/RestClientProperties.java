c5226638b419bbb0884ba3348a9300ea74e46102
DingHao <dh.hiekn@gmail.com>
2023-01-03 15:07:22 +0800

    add RestClientAutoConfiguration

---
+++ mcn-boot-autoconfigure/src/main/java/cn/hiboot/mcn/autoconfigure/web/client/RestClientProperties.java
@@ -0,0 +1,34 @@
+package cn.hiboot.mcn.autoconfigure.web.client;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.time.Duration;
+
+/**
+ * RestTemplateProperties
+ *
+ * @author DingHao
+ * @since 2023/1/3 14:54
+ */
+@ConfigurationProperties(prefix = "rest.template")
+public class RestClientProperties {
+
+    private Duration connectTimeout = Duration.ofMillis(10000);
+    private Duration readTimeout = Duration.ofMillis(60000);
+
+    public Duration getConnectTimeout() {
+        return connectTimeout;
+    }
+
+    public void setConnectTimeout(Duration connectTimeout) {
+        this.connectTimeout = connectTimeout;
+    }
+
+    public Duration getReadTimeout() {
+        return readTimeout;
+    }
+
+    public void setReadTimeout(Duration readTimeout) {
+        this.readTimeout = readTimeout;
+    }
+}
