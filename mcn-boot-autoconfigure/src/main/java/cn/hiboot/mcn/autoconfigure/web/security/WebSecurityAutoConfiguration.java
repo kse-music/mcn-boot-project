9ff9cd0f67dc74540d43476e6a11bf4a9319cbd4
DingHao <dh.hiekn@gmail.com>
2023-02-04 16:01:59 +0800

    add securityExceptionResolver bean

--- mcn-boot-autoconfigure/src/main/java/cn/hiboot/mcn/autoconfigure/web/security/WebSecurityAutoConfiguration.java
+++ mcn-boot-autoconfigure/src/main/java/cn/hiboot/mcn/autoconfigure/web/security/WebSecurityAutoConfiguration.java
@@ -1,11 +1,18 @@
 package cn.hiboot.mcn.autoconfigure.web.security;
 
+import cn.hiboot.mcn.autoconfigure.web.exception.ExceptionResolver;
+import cn.hiboot.mcn.core.exception.ExceptionKeys;
+import cn.hiboot.mcn.core.model.result.RestResp;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * WebSecurityAutoConfiguration
@@ -20,5 +27,22 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
 @Import(IgnoreUrlWebSecurityConfigurer.class)
 public class WebSecurityAutoConfiguration {
+
+    @Bean
+    public ExceptionResolver securityExceptionResolver() {
+        return new ExceptionResolver(){
+
+            @Override
+            public boolean support(HttpServletRequest request, Throwable t) {
+                return t instanceof AccessDeniedException;
+            }
+
+            @Override
+            public RestResp<Object> resolveException(HttpServletRequest request, Throwable t) {
+                return RestResp.error(ExceptionKeys.HTTP_ERROR_403, t.getMessage());
+            }
+
+        };
+    }
 
 }
