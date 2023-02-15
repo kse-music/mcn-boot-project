43f8b9cd50f58194219c6a8e1fa3041accaba833
DingHao <dh.hiekn@gmail.com>
2023-02-15 22:12:20 +0800

    remove checked IoException

--- mcn-boot-autoconfigure/src/main/java/cn/hiboot/mcn/autoconfigure/web/mvc/ResponseUtils.java
+++ mcn-boot-autoconfigure/src/main/java/cn/hiboot/mcn/autoconfigure/web/mvc/ResponseUtils.java
@@ -17,38 +17,40 @@ import java.nio.charset.StandardCharsets;
  */
 public class ResponseUtils {
 
-    public static <T> void success(T data, HttpServletResponse response) throws IOException {
+    public static <T> void success(T data, HttpServletResponse response){
         write(new RestResp<>(data),response);
     }
 
-    public static <T> void success(T data,Long count, HttpServletResponse response) throws IOException {
+    public static <T> void success(T data,Long count, HttpServletResponse response){
         write(new RestResp<>(data,count),response);
     }
 
-    public static void failed(String msg, HttpServletResponse response) throws IOException {
+    public static void failed(String msg, HttpServletResponse response){
         write(RestResp.error(msg),response);
     }
 
-    public static void failed(Integer code,HttpServletResponse response) throws IOException {
+    public static void failed(Integer code,HttpServletResponse response){
         write(RestResp.error(code),response);
     }
 
-    public static void failed(Integer code,String msg, HttpServletResponse response) throws IOException {
+    public static void failed(Integer code,String msg, HttpServletResponse response){
         write(RestResp.error(code,msg),response);
     }
 
-    public static <T> void write(RestResp<T> resp, HttpServletResponse response) throws IOException {
+    public static <T> void write(RestResp<T> resp, HttpServletResponse response){
         write(JacksonUtils.toJson(resp),response);
     }
 
-    public static void write(String str, HttpServletResponse response) throws IOException {
+    public static void write(String str, HttpServletResponse response) {
         response.setContentType(MediaType.APPLICATION_JSON_VALUE);
         response.setCharacterEncoding(StandardCharsets.UTF_8.name());
         response.setStatus(HttpServletResponse.SC_OK);
-        PrintWriter out = response.getWriter();
-        out.write(str);
-        out.flush();
-        out.close();
+        try(PrintWriter out = response.getWriter()){
+            out.write(str);
+            out.flush();
+        } catch (IOException e) {
+            //ignore
+        }
     }
 
 }
