3b3e6ffa0f601d6b3635f2f03a6a1324d13dea91
DingHao <dh.hiekn@gmail.com>
2023-02-28 16:01:33 +0800

    2.3.12.30

---
+++ mcn-cloud-autoconfigure/src/main/java/cn/hiboot/mcn/cloud/security/SessionHolder.java
@@ -0,0 +1,56 @@
+package cn.hiboot.mcn.cloud.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+
+import java.util.Collections;
+import java.util.Map;
+
+/**
+ * SessionHolder
+ *
+ * @author DingHao
+ * @since 2021/8/20 10:32
+ */
+public interface SessionHolder {
+    String BEARER_PREFIX = "Bearer ";
+    String USER_NAME = "user_name";
+
+    static String getUserId() {
+        Object value = getUser().get("userId");
+        return value == null ? null : value.toString();
+    }
+
+    static Map<String, Object> getUser() {
+        Jwt jwt = getJwtToken();
+        return jwt == null ? Collections.emptyMap() : jwt.getClaimAsMap(USER_NAME);
+    }
+
+    static Map<String, Object> getClaims() {
+        Jwt jwt = getJwtToken();
+        return jwt == null ? Collections.emptyMap() : jwt.getClaims();
+    }
+
+    static String getBearerToken() {
+        String token = getJwtTokenString();
+        return token == null ? null : BEARER_PREFIX.concat(token);
+    }
+
+    static String getJwtTokenString() {
+        Jwt jwt = getJwtToken();
+        return jwt == null ? null : jwt.getTokenValue();
+    }
+
+    static Jwt getJwtToken() {
+        JwtAuthenticationToken jwtAuthenticationToken = getJwtAuthenticationToken();
+        return jwtAuthenticationToken == null ? null : jwtAuthenticationToken.getToken();
+    }
+
+    static JwtAuthenticationToken getJwtAuthenticationToken() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication instanceof JwtAuthenticationToken ? (JwtAuthenticationToken) authentication : null;
+    }
+
+}
