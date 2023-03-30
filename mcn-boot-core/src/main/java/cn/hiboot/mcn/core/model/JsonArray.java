9eec60c5d6b35ff6ec0a10c01a2bb68b03bb0d05
DingHao <dh.hiekn@gmail.com>
2023-03-30 20:50:36 +0800

    add rich method to JsonArray

--- mcn-boot-core/src/main/java/cn/hiboot/mcn/core/model/JsonArray.java
+++ mcn-boot-core/src/main/java/cn/hiboot/mcn/core/model/JsonArray.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.JsonNode;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.function.Predicate;
 
 /**
  * JsonArray
@@ -15,14 +16,53 @@ import java.util.List;
 public class JsonArray implements Iterable<JsonObject> {
     private final List<JsonObject> list;
 
+    public JsonArray(){
+        list = new ArrayList<>();
+    }
+
     public JsonArray(JsonNode jsonNode) {
-        list = new ArrayList<>(jsonNode.size());
+        this();
         for (JsonNode node : jsonNode) {
             list.add(new JsonObject(node));
         }
     }
 
-    public JsonObject getJsonObject(int index){
+    public JsonArray add(JsonObject jsonObject){
+        list.add(jsonObject);
+        return this;
+    }
+
+    public JsonArray add(int index,JsonObject jsonObject){
+        list.add(index,jsonObject);
+        return this;
+    }
+
+    public JsonArray remove(JsonObject jsonObject){
+        list.remove(jsonObject);
+        return this;
+    }
+
+    public JsonArray remove(int index){
+        list.remove(index);
+        return this;
+    }
+
+    public JsonArray removeIf(Predicate<JsonObject> filter){
+        list.removeIf(filter);
+        return this;
+    }
+
+    public JsonObject findFirst(Predicate<JsonObject> filter){
+        return list.stream().filter(filter).findFirst().orElse(null);
+    }
+
+    public JsonArray findAll(Predicate<JsonObject> filter){
+        JsonArray jsonArray = new JsonArray();
+        list.stream().filter(filter).forEach(jsonArray::add);
+        return jsonArray;
+    }
+
+    public JsonObject get(int index){
         return list.get(index);
     }
 
