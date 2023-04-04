ca8316da648fe2d76a1724b879700556737468f1
DingHao <dh.hiekn@gmail.com>
2023-04-04 17:46:26 +0800

    support service register in script

--- mcn-cloud-autoconfigure/src/main/java/cn/hiboot/mcn/cloud/discover/ServiceRegisterAutoConfiguration.java
+++ mcn-cloud-autoconfigure/src/main/java/cn/hiboot/mcn/cloud/discover/ServiceRegisterAutoConfiguration.java
@@ -1,14 +1,13 @@
 package cn.hiboot.mcn.cloud.discover;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigureOrder;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnSingleCandidate;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnWarDeployment;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.boot.autoconfigure.condition.*;
 import org.springframework.cloud.client.serviceregistry.AbstractAutoServiceRegistration;
-import org.springframework.context.ApplicationListener;
-import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.annotation.Conditional;
+import org.springframework.context.annotation.Import;
 import org.springframework.core.Ordered;
 
 /**
@@ -20,18 +19,39 @@ import org.springframework.core.Ordered;
 @AutoConfiguration
 @ConditionalOnSingleCandidate(AbstractAutoServiceRegistration.class)
 @AutoConfigureOrder(Ordered.LOWEST_PRECEDENCE)
-@ConditionalOnWarDeployment
-@ConditionalOnWebApplication
-public class ServiceRegisterAutoConfiguration implements ApplicationListener<ContextRefreshedEvent> {
+@Conditional(ServiceRegisterAutoConfiguration.ServiceRegisterCondition.class)
+@Import(ServiceRegisterAutoConfiguration.ServiceRegisterConfig.class)
+@ConditionalOnNotWebApplication
+public class ServiceRegisterAutoConfiguration {
 
-    @Autowired(required = false)
-    private AbstractAutoServiceRegistration<?> registration;
+    static class ServiceRegisterConfig implements BeanPostProcessor {
 
-    @Override
-    public void onApplicationEvent(ContextRefreshedEvent event) {
-        if (registration != null) {
-            registration.start();
+        @Override
+        public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+            if(bean instanceof AbstractAutoServiceRegistration){
+                ((AbstractAutoServiceRegistration<?>) bean).start();
+            }
+            return bean;
         }
+
+    }
+
+    static class ServiceRegisterCondition extends AnyNestedCondition {
+
+        ServiceRegisterCondition() {
+            super(ConfigurationPhase.PARSE_CONFIGURATION);
+        }
+
+        @ConditionalOnWarDeployment
+        static class WarDeploy {
+
+        }
+
+        @ConditionalOnClass(name = "com.huaweicloud.servicecomb.discovery.registry.ServiceCombAutoServiceRegistration")
+        static class CseServiceEngine {
+
+        }
+
     }
 
 }
