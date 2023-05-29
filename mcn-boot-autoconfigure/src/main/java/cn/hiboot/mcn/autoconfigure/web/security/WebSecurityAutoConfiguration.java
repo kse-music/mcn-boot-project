058867ea2582c06c0d7b11c8a43a074f25feaf40
DingHao <dh.hiekn@gmail.com>
2023-05-29 11:38:14 +0800

    Support default ignore url in reactive

--- mcn-boot-autoconfigure/src/main/java/cn/hiboot/mcn/autoconfigure/web/security/WebSecurityAutoConfiguration.java
+++ mcn-boot-autoconfigure/src/main/java/cn/hiboot/mcn/autoconfigure/web/security/WebSecurityAutoConfiguration.java
@@ -9,11 +9,21 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatcher;
+import org.springframework.security.web.server.util.matcher.ServerWebExchangeMatchers;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.security.web.util.matcher.OrRequestMatcher;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import org.springframework.web.server.handler.DefaultWebFilterChain;
+import reactor.core.publisher.Mono;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -30,34 +40,61 @@ import java.util.stream.Collectors;
 @AutoConfiguration
 @ConditionalOnClass(WebSecurity.class)
 @EnableConfigurationProperties(WebSecurityProperties.class)
-@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
 public class WebSecurityAutoConfiguration {
 
-    private final WebSecurityProperties webSecurityProperties;
+    @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
+    @Configuration(proxyBeanMethods = false)
+    static class ServletWebSecurityConfiguration {
 
-    public WebSecurityAutoConfiguration(WebSecurityProperties webSecurityProperties){
-        this.webSecurityProperties = webSecurityProperties;
+        @Bean
+        public WebSecurityCustomizer webSecurityCustomizer(WebSecurityProperties webSecurityProperties){
+            return web -> {
+                List<String> urls = ignoreUrl(webSecurityProperties);
+                if(McnUtils.isNotNullAndEmpty(urls)){
+                    web.ignoring().requestMatchers(new OrRequestMatcher(urls.stream().map(AntPathRequestMatcher::new).collect(Collectors.toList())));
+                }
+            };
+        }
     }
 
-    @Bean
-    public WebSecurityCustomizer webSecurityCustomizer(){
-        return web -> {
-            List<String> urls = new ArrayList<>();
-            if(webSecurityProperties.isEnableDefaultIgnore()){
-                Collections.addAll(urls,webSecurityProperties.getDefaultExcludeUrls());
-            }
-            if(webSecurityProperties.getExcludeUrls() != null){
-                Collections.addAll(urls,webSecurityProperties.getExcludeUrls());
-            }
-            if(McnUtils.isNotNullAndEmpty(urls)){
-                web.ignoring().requestMatchers(new OrRequestMatcher(urls.stream().map(AntPathRequestMatcher::new).collect(Collectors.toList())));
-            }
-        };
+    private static List<String> ignoreUrl(WebSecurityProperties webSecurityProperties){
+        List<String> urls = new ArrayList<>();
+        if(webSecurityProperties.isEnableDefaultIgnore()){
+            Collections.addAll(urls,webSecurityProperties.getDefaultExcludeUrls());
+        }
+        if(webSecurityProperties.getExcludeUrls() != null){
+            Collections.addAll(urls,webSecurityProperties.getExcludeUrls());
+        }
+        return urls;
     }
 
     @Bean
     public ExceptionResolver<AccessDeniedException> securityExceptionResolver() {
         return t -> RestResp.error(ExceptionKeys.HTTP_ERROR_403);
+    }
+
+    @ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.REACTIVE)
+    @Configuration(proxyBeanMethods = false)
+    @Import(ReactiveWebSecurityConfiguration.IgnoreUrlFilter.class)
+    static class ReactiveWebSecurityConfiguration {
+
+        @Order(-101)
+        static class IgnoreUrlFilter implements WebFilter{
+
+            private final ServerWebExchangeMatcher requiresAuthenticationMatcher;
+
+            public IgnoreUrlFilter(WebSecurityProperties webSecurityProperties){
+                this.requiresAuthenticationMatcher = ServerWebExchangeMatchers.pathMatchers(ignoreUrl(webSecurityProperties).toArray(new String[0]));
+            }
+
+            @Override
+            public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+                return requiresAuthenticationMatcher.matches(exchange).filter(ServerWebExchangeMatcher.MatchResult::isMatch)
+                        .switchIfEmpty(chain.filter(exchange).then(Mono.empty())).flatMap(m -> ((DefaultWebFilterChain) chain).getHandler().handle(exchange));
+            }
+
+        }
+
     }
 
 }
