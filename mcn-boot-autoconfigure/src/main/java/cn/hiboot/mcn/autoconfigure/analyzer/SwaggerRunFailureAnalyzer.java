c10bc573652fde8bf97fcbe26df664e9056c7624
DingHao <dh.hiekn@gmail.com>
2023-09-18 17:43:37 +0800

    add SwaggerRunFailureAnalyzer

--- mcn-boot-autoconfigure/src/main/java/cn/hiboot/mcn/autoconfigure/analyzer/SwaggerRunFailureAnalyzer.java
+++ mcn-boot-autoconfigure/src/main/java/cn/hiboot/mcn/autoconfigure/analyzer/SwaggerRunFailureAnalyzer.java
@@ -11,11 +11,15 @@ import org.springframework.util.ClassUtils;
  * @since 2023/9/18 17:15
  */
 class SwaggerRunFailureAnalyzer extends AbstractFailureAnalyzer<IllegalStateException> {
-    private static final String WEBFLUX_INDICATOR_CLASS = "org.springframework.web.reactive.DispatcherHandler";
+    private static final boolean WEBFLUX_CLASS_EXIST;
+
+    static {
+        WEBFLUX_CLASS_EXIST = ClassUtils.isPresent("org.springframework.web.reactive.DispatcherHandler", null);
+    }
 
     @Override
     protected FailureAnalysis analyze(Throwable rootFailure, IllegalStateException cause) {
-        if(ClassUtils.isPresent(WEBFLUX_INDICATOR_CLASS, null) && cause.getMessage().equals("Error processing condition on cn.hiboot.mcn.autoconfigure.web.swagger.SwaggerAutoConfiguration.openApi")){
+        if(WEBFLUX_CLASS_EXIST && cause.getMessage().equals("Error processing condition on cn.hiboot.mcn.autoconfigure.web.swagger.SwaggerAutoConfiguration.openApi")){
             return new FailureAnalysis("need springdoc-openapi-starter-webflux-ui dependency", getAction(), cause);
         }
         return null;
