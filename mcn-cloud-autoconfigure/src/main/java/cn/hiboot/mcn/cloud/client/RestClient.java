8c2ea31772a7e9b875555614dccfc4d462bb2403
DingHao <dh.hiekn@gmail.com>
2023-10-26 14:56:59 +0800

    RestClient support set header
    (cherry picked from commit 975a6a6ef6953deba8fac63cacfd2c667b196aff)

--- mcn-cloud-autoconfigure/src/main/java/cn/hiboot/mcn/cloud/client/RestClient.java
+++ mcn-cloud-autoconfigure/src/main/java/cn/hiboot/mcn/cloud/client/RestClient.java
@@ -15,6 +15,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
 
 /**
  * RestClient
@@ -32,165 +33,292 @@ public class RestClient {
         this(new RestTemplate());
     }
 
+    public RestClient(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
     protected RestClient(Class<?> wrapperClass) {
         this();
         this.wrapperClass = wrapperClass;
     }
 
-    public RestClient(RestTemplate restTemplate) {
-        this.restTemplate = restTemplate;
-    }
-
     protected RestClient(Class<?> wrapperClass, RestTemplate restTemplate) {
+        this(restTemplate);
         this.wrapperClass = wrapperClass;
-        this.restTemplate = restTemplate;
     }
 
     public <D> D getObject(String url, Class<D> resultClass) {
-        return getObject(url, resultClass, null);
+        return getObject(url, resultClass, null, null);
     }
 
-    public <D> D getObject(String url, Class<D> resultClass,Map<String,Object> uriVariables) {
-        return get(url, ResolvableType.forClass(resultClass),uriVariables);
+    public <D> D getObject(String url, Class<D> resultClass, Consumer<HttpHeaders> consumer) {
+        return getObject(url, resultClass, consumer, null);
     }
 
-    public Map<String,Object> getMap(String url) {
-        return getMap(url,null);
+    public <D> D getObject(String url, Class<D> resultClass, Map<String, Object> uriVariables) {
+        return getObject(url, resultClass, null, uriVariables);
     }
 
-    public Map<String,Object> getMap(String url, Map<String,Object> uriVariables) {
-        return getMap(url, String.class, Object.class,uriVariables);
+    public <D> D getObject(String url, Class<D> resultClass, Consumer<HttpHeaders> consumer, Map<String, Object> uriVariables) {
+        return get(url, forClass(resultClass), consumer, uriVariables);
     }
 
-    public <K,V> Map<K,V> getMap(String url,Class<K> key, Class<V> value,Map<String,Object> uriVariables) {
-        return get(url, ResolvableType.forClassWithGenerics(Map.class,key, value),uriVariables);
+    public Map<String, Object> getMap(String url) {
+        return getMap(url, null, null);
+    }
+
+    public Map<String, Object> getMap(String url, Consumer<HttpHeaders> consumer) {
+        return getMap(url, consumer, null);
+    }
+
+    public Map<String, Object> getMap(String url, Map<String, Object> uriVariables) {
+        return getMap(url, null, uriVariables);
+    }
+
+    public Map<String, Object> getMap(String url, Consumer<HttpHeaders> consumer, Map<String, Object> uriVariables) {
+        return getMap(url, String.class, Object.class, consumer, uriVariables);
+    }
+
+    public <K, V> Map<K, V> getMap(String url, Class<K> key, Class<V> value, Map<String, Object> uriVariables) {
+        return getMap(url, key, value, null, uriVariables);
+    }
+
+    public <K, V> Map<K, V> getMap(String url, Class<K> key, Class<V> value, Consumer<HttpHeaders> consumer) {
+        return getMap(url, key, value, consumer, null);
+    }
+
+    public <K, V> Map<K, V> getMap(String url, Class<K> key, Class<V> value, Consumer<HttpHeaders> consumer, Map<String, Object> uriVariables) {
+        return get(url, forClassWithGenerics(Map.class, key, value), consumer, uriVariables);
     }
 
     public <D> List<D> getList(String url, Class<D> resultClass) {
-        return getList(url, resultClass, null);
+        return getList(url, resultClass, null, null);
     }
 
-    public <D> List<D> getList(String url, Class<D> resultClass, Map<String,Object> uriVariables) {
-        return get(url, ResolvableType.forClassWithGenerics(List.class,resultClass),uriVariables);
+    public <D> List<D> getList(String url, Class<D> resultClass, Consumer<HttpHeaders> consumer) {
+        return getList(url, resultClass, consumer, null);
     }
 
-    public List<Map<String,Object>> getListMap(String url) {
-        return getListMap(url, null);
+    public <D> List<D> getList(String url, Class<D> resultClass, Map<String, Object> uriVariables) {
+        return getList(url, resultClass, null, uriVariables);
     }
 
-    public List<Map<String,Object>> getListMap(String url,Map<String,Object> uriVariables) {
-        return getListMap(url, String.class, Object.class,uriVariables);
+    public <D> List<D> getList(String url, Class<D> resultClass, Consumer<HttpHeaders> consumer, Map<String, Object> uriVariables) {
+        return get(url, forClassWithGenerics(List.class, resultClass), consumer, uriVariables);
     }
 
-    public <K,V> List<Map<String,Object>> getListMap(String url,Class<K> key, Class<V> value,Map<String,Object> uriVariables) {
-        return get(url, ResolvableType.forClassWithGenerics(List.class,ResolvableType.forClassWithGenerics(Map.class,key, value)),uriVariables);
+    public List<Map<String, Object>> getListMap(String url) {
+        return getListMap(url, null, null);
     }
 
-    public <D> D get(String url, ResolvableType resultType, Map<String,Object> uriVariables) {
-        return exchange(url, HttpMethod.GET, MediaType.APPLICATION_JSON, resultType, null,uriVariables);
+    public List<Map<String, Object>> getListMap(String url, Map<String, Object> uriVariables) {
+        return getListMap(url, null, uriVariables);
+    }
+
+    public List<Map<String, Object>> getListMap(String url, Consumer<HttpHeaders> consumer) {
+        return getListMap(url, consumer, null);
+    }
+
+    public List<Map<String, Object>> getListMap(String url, Consumer<HttpHeaders> consumer, Map<String, Object> uriVariables) {
+        return getListMap(url, String.class, Object.class, consumer, uriVariables);
+    }
+
+    public <K, V> List<Map<K, V>> getListMap(String url, Class<K> key, Class<V> value, Consumer<HttpHeaders> consumer) {
+        return getListMap(url, key, value, consumer, null);
+    }
+
+    public <K, V> List<Map<K, V>> getListMap(String url, Class<K> key, Class<V> value, Map<String, Object> uriVariables) {
+        return getListMap(url, key, value, null, uriVariables);
+    }
+
+    public <K, V> List<Map<K, V>> getListMap(String url, Class<K> key, Class<V> value, Consumer<HttpHeaders> consumer, Map<String, Object> uriVariables) {
+        return get(url, forClassWithGenerics(List.class, forClassWithGenerics(Map.class, key, value)), consumer, uriVariables);
+    }
+
+    public <D> D get(String url, ResolvableType resultType, Map<String, Object> uriVariables) {
+        return get(url, resultType, null, uriVariables);
+    }
+
+    public <D> D get(String url, ResolvableType resultType, Consumer<HttpHeaders> consumer) {
+        return get(url, resultType, consumer, null);
+    }
+
+    public <D> D get(String url, ResolvableType resultType, Consumer<HttpHeaders> consumer, Map<String, Object> uriVariables) {
+        return exchange(url, HttpMethod.GET, consumer, resultType, null, uriVariables);
+    }
+
+    private ResolvableType forClass(Class<?> clazz) {
+        return ResolvableType.forClass(clazz);
+    }
+
+    private ResolvableType forClassWithGenerics(Class<?> clazz, Class<?>... generics) {
+        return ResolvableType.forClassWithGenerics(clazz, generics);
+    }
+
+    private ResolvableType forClassWithGenerics(Class<?> clazz, ResolvableType... generics) {
+        return ResolvableType.forClassWithGenerics(clazz, generics);
     }
 
     public <A, D> D postObject(String url, Class<D> resultClass, A requestBody) {
-        return postObject(url, resultClass, requestBody,null);
+        return postObject(url, resultClass, requestBody, null, null);
     }
 
-    public <A, D> D postObject(String url, Class<D> resultClass, A requestBody, Map<String,Object> uriVariables) {
-        return post(url,ResolvableType.forClass(resultClass), requestBody,uriVariables);
+    public <A, D> D postObject(String url, Class<D> resultClass, A requestBody, Consumer<HttpHeaders> consumer) {
+        return postObject(url, resultClass, requestBody, consumer, null);
     }
 
-    public <A> Map<String,Object> postMap(String url, A requestBody) {
-        return postMap(url,requestBody,null);
+    public <A, D> D postObject(String url, Class<D> resultClass, A requestBody, Map<String, Object> uriVariables) {
+        return postObject(url, resultClass, requestBody, null, uriVariables);
     }
 
-    public <A> Map<String,Object> postMap(String url, A requestBody, Map<String,Object> uriVariables) {
-        return postMap(url, String.class, Object.class,requestBody,uriVariables);
+    public <A, D> D postObject(String url, Class<D> resultClass, A requestBody, Consumer<HttpHeaders> consumer, Map<String, Object> uriVariables) {
+        return post(url, forClass(resultClass), requestBody, consumer, uriVariables);
     }
 
-    public <A,K,V> Map<K,V> postMap(String url,Class<K> key, Class<V> value,A requestBody,Map<String,Object> uriVariables) {
-        return post(url, ResolvableType.forClassWithGenerics(Map.class,key, value),requestBody,uriVariables);
+    public <A> Map<String, Object> postMap(String url, A requestBody) {
+        return postMap(url, requestBody, null, null);
+    }
+
+    public <A> Map<String, Object> postMap(String url, A requestBody, Consumer<HttpHeaders> consumer) {
+        return postMap(url, requestBody, consumer, null);
+    }
+
+    public <A> Map<String, Object> postMap(String url, A requestBody, Map<String, Object> uriVariables) {
+        return postMap(url, requestBody, null, uriVariables);
+    }
+
+    public <A> Map<String, Object> postMap(String url, A requestBody, Consumer<HttpHeaders> consumer, Map<String, Object> uriVariables) {
+        return postMap(url, String.class, Object.class, requestBody, consumer, uriVariables);
+    }
+
+    public <A, K, V> Map<K, V> postMap(String url, Class<K> key, Class<V> value, A requestBody, Consumer<HttpHeaders> consumer) {
+        return postMap(url, key, value, requestBody, consumer, null);
+    }
+
+    public <A, K, V> Map<K, V> postMap(String url, Class<K> key, Class<V> value, A requestBody, Map<String, Object> uriVariables) {
+        return postMap(url, key, value, requestBody, null, uriVariables);
+    }
+
+    public <A, K, V> Map<K, V> postMap(String url, Class<K> key, Class<V> value, A requestBody, Consumer<HttpHeaders> consumer, Map<String, Object> uriVariables) {
+        return post(url, forClassWithGenerics(Map.class, key, value), requestBody, consumer, uriVariables);
     }
 
     public <A, D> List<D> postList(String url, Class<D> resultClass, A requestBody) {
-        return postList(url, resultClass, requestBody,null);
+        return postList(url, resultClass, requestBody, null, null);
     }
 
-    public <A, D> List<D> postList(String url, Class<D> resultClass, A requestBody, Map<String,Object> uriVariables) {
-        return post(url,ResolvableType.forClassWithGenerics(List.class,resultClass), requestBody,uriVariables);
+    public <A, D> List<D> postList(String url, Class<D> resultClass, A requestBody, Consumer<HttpHeaders> consumer) {
+        return postList(url, resultClass, requestBody, consumer, null);
     }
 
-    public <A> List<Map<String,Object>> postListMap(String url, A requestBody) {
-        return postListMap(url,requestBody,null);
+    public <A, D> List<D> postList(String url, Class<D> resultClass, A requestBody, Map<String, Object> uriVariables) {
+        return postList(url, resultClass, requestBody, null, uriVariables);
     }
 
-    public <A> List<Map<String,Object>> postListMap(String url, A requestBody, Map<String,Object> uriVariables) {
-        return postListMap(url, String.class, Object.class,requestBody,uriVariables);
+    public <A, D> List<D> postList(String url, Class<D> resultClass, A requestBody, Consumer<HttpHeaders> consumer, Map<String, Object> uriVariables) {
+        return post(url, forClassWithGenerics(List.class, resultClass), requestBody, consumer, uriVariables);
     }
 
-    public <A,K,V> List<Map<K,V>> postListMap(String url,Class<K> key, Class<V> value,A requestBody,Map<String,Object> uriVariables) {
-        return post(url, ResolvableType.forClassWithGenerics(List.class,ResolvableType.forClassWithGenerics(Map.class,key, value)),requestBody,uriVariables);
+    public <A> List<Map<String, Object>> postListMap(String url, A requestBody) {
+        return postListMap(url, requestBody, null, null);
     }
 
-    public <A, D> D post(String url, ResolvableType resultType, A requestBody, Map<String,Object> uriVariables) {
-        return exchange(url, HttpMethod.POST, MediaType.APPLICATION_JSON, resultType, requestBody,uriVariables);
+    public <A> List<Map<String, Object>> postListMap(String url, A requestBody, Consumer<HttpHeaders> consumer) {
+        return postListMap(url, requestBody, consumer, null);
     }
 
-    public <A, D> D exchange(String url, HttpMethod method, MediaType mediaType, ResolvableType resultType, A requestBody, Map<String,Object> uriVariables) {
-        return doExchange(url, method, mediaType, resultType, requestBody, uriVariables);
+    public <A> List<Map<String, Object>> postListMap(String url, A requestBody, Map<String, Object> uriVariables) {
+        return postListMap(url, requestBody, null, uriVariables);
     }
 
-    private <A, D, W> D doExchange(String url, HttpMethod method, MediaType mediaType, ResolvableType resultType, A requestBody, Map<String, ?> uriVariables) {
-        if(uriVariables == null){
+    public <A> List<Map<String, Object>> postListMap(String url, A requestBody, Consumer<HttpHeaders> consumer, Map<String, Object> uriVariables) {
+        return postListMap(url, String.class, Object.class, requestBody, consumer, uriVariables);
+    }
+
+    public <A, K, V> List<Map<K, V>> postListMap(String url, Class<K> key, Class<V> value, A requestBody, Consumer<HttpHeaders> consumer) {
+        return postListMap(url, key, value, requestBody, consumer, null);
+    }
+
+    public <A, K, V> List<Map<K, V>> postListMap(String url, Class<K> key, Class<V> value, A requestBody, Map<String, Object> uriVariables) {
+        return postListMap(url, key, value, requestBody, null, uriVariables);
+    }
+
+    public <A, K, V> List<Map<K, V>> postListMap(String url, Class<K> key, Class<V> value, A requestBody, Consumer<HttpHeaders> consumer, Map<String, Object> uriVariables) {
+        return post(url, forClassWithGenerics(List.class, forClassWithGenerics(Map.class, key, value)), requestBody, consumer, uriVariables);
+    }
+
+    public <A, D> D post(String url, ResolvableType resultType, A requestBody, Consumer<HttpHeaders> consumer) {
+        return post(url, resultType, requestBody, consumer, null);
+    }
+
+    public <A, D> D post(String url, ResolvableType resultType, A requestBody, Map<String, Object> uriVariables) {
+        return post(url, resultType, requestBody, null, uriVariables);
+    }
+
+    public <A, D> D post(String url, ResolvableType resultType, A requestBody, Consumer<HttpHeaders> consumer, Map<String, Object> uriVariables) {
+        return exchange(url, HttpMethod.POST, consumer, resultType, requestBody, uriVariables);
+    }
+
+    public <A, D> D exchange(String url, HttpMethod method, Consumer<HttpHeaders> headersConsumer, ResolvableType resultType, A requestBody, Map<String, Object> uriVariables) {
+        return doExchange(url, method, headersConsumer, resultType, requestBody, uriVariables);
+    }
+
+    private <A, D, W> D doExchange(String url, HttpMethod method, Consumer<HttpHeaders> headersConsumer, ResolvableType resultType, A requestBody, Map<String, ?> uriVariables) {
+        if (uriVariables == null) {
             uriVariables = Collections.emptyMap();
         }
+        if (headersConsumer == null) {
+            headersConsumer = headers -> headers.setContentType(MediaType.APPLICATION_JSON);
+        }
         HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(mediaType);
+        headersConsumer.accept(headers);
         HttpEntity<A> entity = new HttpEntity<>(requestBody, headers);
         long startTime = System.currentTimeMillis();
         ResponseEntity<W> responseEntity;
         try {
-            responseEntity = restTemplate.exchange(url, method, entity, ParameterizedTypeReference.forType(ResolvableType.forClassWithGenerics(wrapperClass, resultType).getType()),uriVariables);
+            responseEntity = restTemplate.exchange(url, method, entity, ParameterizedTypeReference.forType(ResolvableType.forClassWithGenerics(wrapperClass, resultType).getType()), uriVariables);
         } catch (RestClientException e) {
             logError(url, requestBody, e.getMessage());
             throw ServiceException.newInstance(ExceptionKeys.REMOTE_SERVICE_ERROR);
-        }finally {
-            if(log.isDebugEnabled()) {
+        } finally {
+            if (log.isDebugEnabled()) {
                 log.debug("url={}, cost time={}ms, inputParam={}", url, System.currentTimeMillis() - startTime, JacksonUtils.toJson(requestBody));
             }
         }
-        return processResponse(responseEntity,url,requestBody);
+        return processResponse(responseEntity, url, requestBody);
     }
 
-    protected <A,D,W> D processResponse(ResponseEntity<W> responseEntity,String url,A requestBody){
+    protected <A, D, W> D processResponse(ResponseEntity<W> responseEntity, String url, A requestBody) {
         if (responseEntity.getStatusCode() != HttpStatus.OK) {
             throw ServiceException.newInstance(ExceptionKeys.REMOTE_SERVICE_ERROR);
         }
-        try{
-            if(responseEntity.hasBody()){
+        try {
+            if (responseEntity.hasBody()) {
                 D response = extractData(responseEntity.getBody());
-                if(log.isDebugEnabled()){
-                    log.debug("response={}",JacksonUtils.toJson(response));
+                if (log.isDebugEnabled()) {
+                    log.debug("response={}", JacksonUtils.toJson(response));
                 }
                 return response;
             }
             return null;
-        }catch (ServiceException e){
+        } catch (ServiceException e) {
             logError(url, requestBody, e.getMessage());
             throw e;
         }
     }
 
-    @SuppressWarnings({"unchecked","rawtypes"})
-    protected <D,W> D extractData(W body){
-        RestResp restResp = (RestResp)body;
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    protected <D, W> D extractData(W body) {
+        RestResp restResp = (RestResp) body;
         if (restResp.isFailed()) {
-            throw ServiceException.newInstance(ExceptionKeys.REMOTE_SERVICE_ERROR,restResp.getErrorInfo());
+            throw ServiceException.newInstance(ExceptionKeys.REMOTE_SERVICE_ERROR, restResp.getErrorInfo());
         }
         return (D) restResp.getData();
     }
 
-    private void logError(String url, Object requestBody, String errorMsg){
-        if(log.isDebugEnabled()){
-            log.error("url={}, inputParam={}, errorInfo={}", url,JacksonUtils.toJson(requestBody), errorMsg);
+    private void logError(String url, Object requestBody, String errorMsg) {
+        if (log.isDebugEnabled()) {
+            log.error("url={}, inputParam={}, errorInfo={}", url, JacksonUtils.toJson(requestBody), errorMsg);
         }
     }
 
