f1ec54780f63931ad5a219c2ff364172d37af4dd
DingHao <dh.hiekn@gmail.com>
2024-07-11 14:21:25 +0800

    Add exist method to minio

--- mcn-boot-autoconfigure/src/main/java/cn/hiboot/mcn/autoconfigure/minio/Minio.java
+++ mcn-boot-autoconfigure/src/main/java/cn/hiboot/mcn/autoconfigure/minio/Minio.java
@@ -35,127 +35,127 @@ public interface Minio {
         return getFilePath(getDefaultBucketName(), fileName);
     }
 
-    default String getFilePath(String bucketName,String fileName) {
-        return String.format("%s/%s/%s", getMinioClient().getConfig().getEndpoint(), bucketName , fileName);
+    default String getFilePath(String bucketName, String fileName) {
+        return String.format("%s/%s/%s", getMinioClient().getConfig().getEndpoint(), bucketName, fileName);
     }
 
-    default String getDefaultBucketName(){
+    default String getDefaultBucketName() {
         return getMinioClient().getConfig().getDefaultBucketName();
     }
 
-    default String getOrDefaultBucket(String bucketName){
-        if(McnUtils.isNullOrEmpty(bucketName)){
+    default String getOrDefaultBucket(String bucketName) {
+        if (McnUtils.isNullOrEmpty(bucketName)) {
             bucketName = getDefaultBucketName();
         }
-        if(McnUtils.isNullOrEmpty(bucketName)){
+        if (McnUtils.isNullOrEmpty(bucketName)) {
             throw new MinioException(bucketName + " must not be null or empty");
         }
         return bucketName;
     }
 
-    default void upload(String objectName,long objectSize,InputStream stream){
-        upload(objectName,objectSize,null,stream);
+    default void upload(String objectName, long objectSize, InputStream stream) {
+        upload(objectName, objectSize, null, stream);
     }
 
-    default void upload(String objectName,long objectSize,String contentType,InputStream stream){
-        upload(null,objectName,objectSize,0,contentType,stream);
+    default void upload(String objectName, long objectSize, String contentType, InputStream stream) {
+        upload(null, objectName, objectSize, 0, contentType, stream);
     }
 
     /**
      * 文件上传
      *
-     * @param bucketName 桶名称
-     * @param objectName 文件名
-     * @param objectSize 文件大小
-     * @param partSize 分片大小
+     * @param bucketName  桶名称
+     * @param objectName  文件名
+     * @param objectSize  文件大小
+     * @param partSize    分片大小
      * @param contentType contentType
-     * @param stream   文件流
+     * @param stream      文件流
      */
-    default void upload(String bucketName,String objectName,long objectSize,long partSize,String contentType, InputStream stream) {
+    default void upload(String bucketName, String objectName, long objectSize, long partSize, String contentType, InputStream stream) {
         bucketName = getOrDefaultBucket(bucketName);
         DefaultMinioClient minioClient = getMinioClient();
-        if(partSize == 0){
+        if (partSize == 0) {
             partSize = minioClient.getConfig().getMinMultipartSize().toBytes();
         }
-        try{
+        try {
             PutObjectArgs.Builder builder = PutObjectArgs.builder()
                     .bucket(bucketName)
                     .object(objectName)
                     .headers(map)
                     .stream(stream, objectSize, partSize);
-            if(McnUtils.isNotNullAndEmpty(contentType)){
+            if (McnUtils.isNotNullAndEmpty(contentType)) {
                 builder.contentType(contentType);
             }
             minioClient.putObject(builder.build()).get();
-        }catch (Exception e){
-            throw new MinioException(e);
-        }
-    }
-
-    default void uploadParallel(String objectName,long objectSize,InputStream stream){
-        uploadParallel(objectName,objectSize,null,stream);
-    }
-
-    default void uploadParallel(String objectName,long objectSize,String contentType,InputStream stream){
-        uploadParallel(null,objectName,objectSize,contentType,stream);
-    }
-
-    default void uploadParallel(String bucketName,String objectName,long objectSize,String contentType, InputStream stream){
-        bucketName = getOrDefaultBucket(bucketName);
-        try {
-            getMinioClient().upload(bucketName,objectName,objectSize,contentType,stream);
         } catch (Exception e) {
             throw new MinioException(e);
         }
     }
 
-    default PreSignResult presignedObjectUrl(String objectName,String uploadId,int count){
-        return presignedObjectUrl(null,objectName,uploadId,null,count);
+    default void uploadParallel(String objectName, long objectSize, InputStream stream) {
+        uploadParallel(objectName, objectSize, null, stream);
     }
 
-    default PreSignResult presignedObjectUrl(String objectName,String uploadId,String contentType, int count){
-        return presignedObjectUrl(null,objectName,uploadId,contentType,count);
+    default void uploadParallel(String objectName, long objectSize, String contentType, InputStream stream) {
+        uploadParallel(null, objectName, objectSize, contentType, stream);
     }
 
-    default PreSignResult presignedObjectUrl(String bucketName,String objectName,String uploadId,String contentType, int count){
+    default void uploadParallel(String bucketName, String objectName, long objectSize, String contentType, InputStream stream) {
         bucketName = getOrDefaultBucket(bucketName);
         try {
-            return getMinioClient().getPresignedObjectUrl(bucketName,objectName,uploadId,contentType,count);
+            getMinioClient().upload(bucketName, objectName, objectSize, contentType, stream);
         } catch (Exception e) {
             throw new MinioException(e);
         }
     }
 
-    default List<Integer> listParts(String objectName, String uploadId){
-        return listParts(null,objectName,uploadId);
+    default PreSignResult presignedObjectUrl(String objectName, String uploadId, int count) {
+        return presignedObjectUrl(null, objectName, uploadId, null, count);
     }
 
-    default List<Integer> listParts(String bucketName, String objectName, String uploadId){
+    default PreSignResult presignedObjectUrl(String objectName, String uploadId, String contentType, int count) {
+        return presignedObjectUrl(null, objectName, uploadId, contentType, count);
+    }
+
+    default PreSignResult presignedObjectUrl(String bucketName, String objectName, String uploadId, String contentType, int count) {
         bucketName = getOrDefaultBucket(bucketName);
         try {
-            return getMinioClient().listParts(bucketName,objectName,uploadId);
+            return getMinioClient().getPresignedObjectUrl(bucketName, objectName, uploadId, contentType, count);
         } catch (Exception e) {
             throw new MinioException(e);
         }
     }
 
-    default void mergeMultipartUpload(String objectName, String uploadId){
-        mergeMultipartUpload(null,objectName,uploadId);
+    default List<Integer> listParts(String objectName, String uploadId) {
+        return listParts(null, objectName, uploadId);
     }
 
-    default void mergeMultipartUpload(String bucketName,String objectName, String uploadId){
+    default List<Integer> listParts(String bucketName, String objectName, String uploadId) {
         bucketName = getOrDefaultBucket(bucketName);
         try {
-            getMinioClient().mergeMultipartUpload(bucketName,objectName,uploadId);
+            return getMinioClient().listParts(bucketName, objectName, uploadId);
+        } catch (Exception e) {
+            throw new MinioException(e);
+        }
+    }
+
+    default void mergeMultipartUpload(String objectName, String uploadId) {
+        mergeMultipartUpload(null, objectName, uploadId);
+    }
+
+    default void mergeMultipartUpload(String bucketName, String objectName, String uploadId) {
+        bucketName = getOrDefaultBucket(bucketName);
+        try {
+            getMinioClient().mergeMultipartUpload(bucketName, objectName, uploadId);
         } catch (MinioException e) {
             throw e;
-        }catch (Exception e) {
+        } catch (Exception e) {
             throw new MinioException(e);
         }
     }
 
-    default void delete(String objectName){
-        delete(null,objectName);
+    default void delete(String objectName) {
+        delete(null, objectName);
     }
 
     /**
@@ -166,39 +166,39 @@ public interface Minio {
      */
     default void delete(String bucketName, String objectName) {
         bucketName = getOrDefaultBucket(bucketName);
-        try{
+        try {
             RemoveObjectArgs args = RemoveObjectArgs.builder()
                     .bucket(bucketName)
                     .object(objectName)
                     .build();
             getMinioClient().removeObject(args).get();
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new MinioException(e);
         }
     }
 
-    default List<Item> listObjects(){
-        return listObjects(null,false);
+    default List<Item> listObjects() {
+        return listObjects(null, false);
     }
 
-    default List<Item> listObjects(boolean recursive){
-        return listObjects(null,recursive);
+    default List<Item> listObjects(boolean recursive) {
+        return listObjects(null, recursive);
     }
 
     default List<Item> listObjects(String bucketName) {
-        return listObjects(bucketName,false);
+        return listObjects(bucketName, false);
     }
 
     /**
      * 文件列表
      *
      * @param bucketName 桶名称
-     * @param recursive 列出所有文件包括子文件
+     * @param recursive  列出所有文件包括子文件
      * @return item
      */
-    default List<Item> listObjects(String bucketName,boolean recursive) {
+    default List<Item> listObjects(String bucketName, boolean recursive) {
         bucketName = getOrDefaultBucket(bucketName);
-        try{
+        try {
             ListObjectsArgs args = ListObjectsArgs.builder().bucket(bucketName).recursive(recursive).build();
             Iterable<Result<Item>> list = getMinioClient().listObjects(args);
             return StreamSupport.stream(list.spliterator(), true).map(r -> {
@@ -208,18 +208,18 @@ public interface Minio {
                 }
                 return null;
             }).filter(Objects::nonNull).collect(Collectors.toList());
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new MinioException(e);
         }
     }
 
-    default void deleteAll(){
+    default void deleteAll() {
         deleteAll(null);
     }
 
     default void deleteAll(String bucketName) {
         bucketName = getOrDefaultBucket(bucketName);
-        try{
+        try {
             ListObjectsArgs args = ListObjectsArgs.builder().bucket(bucketName).recursive(true).build();
             Iterable<Result<Item>> list = getMinioClient().listObjects(args);
             List<DeleteObject> objectList = StreamSupport.stream(list.spliterator(), true).map(r -> {
@@ -231,20 +231,21 @@ public interface Minio {
             }).filter(Objects::nonNull).collect(Collectors.toList());
             for (Result<DeleteError> errorResult : getMinioClient().removeObjects(RemoveObjectsArgs.builder().bucket(bucketName).objects(objectList).build())) {
                 DeleteError error = errorResult.get();
-                log.error("Error delete object {} ; Reason is {}", error.objectName(),error.message());
+                log.error("Error delete object {} ; Reason is {}", error.objectName(), error.message());
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new MinioException(e);
         }
     }
 
-    default InputStream getObject(String objectName){
-        return getObject(objectName,null,null);
+    default InputStream getObject(String objectName) {
+        return getObject(objectName, null, null);
     }
 
-    default InputStream getObject(String objectName,Long offset,Long length) {
-        return getObject(null,objectName,offset,length);
+    default InputStream getObject(String objectName, Long offset, Long length) {
+        return getObject(null, objectName, offset, length);
     }
+
     /**
      * 获取文件
      *
@@ -252,9 +253,9 @@ public interface Minio {
      * @param objectName 文件名称
      * @return 二进制流
      */
-    default InputStream getObject(String bucketName, String objectName,Long offset,Long length) {
+    default InputStream getObject(String bucketName, String objectName, Long offset, Long length) {
         bucketName = getOrDefaultBucket(bucketName);
-        try{
+        try {
             GetObjectArgs args = GetObjectArgs.builder()
                     .bucket(bucketName)
                     .object(objectName)
@@ -262,45 +263,46 @@ public interface Minio {
                     .length(length)
                     .build();
             return getMinioClient().getObject(args).get();
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new MinioException(e);
         }
     }
 
-    default boolean buckExist(String bucketName){
+    default boolean buckExist(String bucketName) {
         try {
             return getMinioClient().bucketExists(BucketExistsArgs.builder().bucket(bucketName).build()).get();
         } catch (Exception e) {
-            log.error("check buck exist error : {}",e.getMessage());
+            log.error("check buck exist error : {}", e.getMessage());
         }
         return false;
     }
 
     /**
      * 创建bucket
+     *
      * @param bucketName bucket名称
      */
     default void createBucket(String bucketName) {
-        if(buckExist(bucketName)){
+        if (buckExist(bucketName)) {
             return;
         }
-        try{
+        try {
             MakeBucketArgs args = MakeBucketArgs.builder()
                     .bucket(bucketName)
                     .build();
             getMinioClient().makeBucket(args).get();
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new MinioException(e);
         }
     }
 
     default List<BucketItem> listBuckets() {
-        try{
+        try {
             List<Bucket> buckets = getMinioClient().listBuckets().get();
-            if(McnUtils.isNotNullAndEmpty(buckets)){
-                return buckets.stream().map(b -> new BucketItem(b.name(),b.creationDate())).collect(Collectors.toList());
+            if (McnUtils.isNotNullAndEmpty(buckets)) {
+                return buckets.stream().map(b -> new BucketItem(b.name(), b.creationDate())).collect(Collectors.toList());
             }
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new MinioException(e);
         }
         return Collections.emptyList();
@@ -309,16 +311,28 @@ public interface Minio {
     /**
      * 删除bucket
      * 该文件夹下不能有文件或子文件夹
+     *
      * @param bucketName bucket名称
      */
     default void deleteBucket(String bucketName) {
-        try{
+        try {
             RemoveBucketArgs args = RemoveBucketArgs.builder()
                     .bucket(bucketName)
                     .build();
             getMinioClient().removeBucket(args).get();
-        }catch (Exception e){
+        } catch (Exception e) {
             throw new MinioException(e);
+        }
+    }
+
+    default boolean exist(String filename) {
+        try {
+            getMinioClient().statObject(StatObjectArgs.builder()
+                    .bucket(getDefaultBucketName())
+                    .object(filename).build()).get();
+            return true;
+        } catch (Exception e) {
+            return false;
         }
     }
 
