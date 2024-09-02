5abb98cf3abc279e351df4bf66a966cf6bbb5594
DingHao <dh.hiekn@gmail.com>
2024-09-02 15:35:44 +0800

    AccumulateExecutor implements Closeable

--- mcn-boot-core/src/main/java/cn/hiboot/mcn/core/task/AccumulateExecutor.java
+++ mcn-boot-core/src/main/java/cn/hiboot/mcn/core/task/AccumulateExecutor.java
@@ -2,6 +2,7 @@ package cn.hiboot.mcn.core.task;
 
 import cn.hiboot.mcn.core.util.McnAssert;
 
+import java.io.Closeable;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
@@ -12,21 +13,27 @@ import java.util.function.Consumer;
  * @author DingHao
  * @since 2024/8/6 10:57
  */
-public final class AccumulateExecutor<T> {
+public final class AccumulateExecutor<T> implements Closeable {
 
     private final TaskThreadPool taskThreadPool;
     private List<T> datum;
     private final Consumer<List<T>> consumer;
     private final int batchSize;
+    private final boolean syncExecute;
 
     public AccumulateExecutor(Consumer<List<T>> consumer) {
         this(1000, consumer);
     }
 
     public AccumulateExecutor(int batchSize, Consumer<List<T>> consumer) {
+        this(batchSize, consumer, false);
+    }
+
+    public AccumulateExecutor(int batchSize, Consumer<List<T>> consumer, boolean syncExecute) {
         McnAssert.state(batchSize > 0, "batchSize must gt 0");
         McnAssert.notNull(consumer, "consumer must not be null");
         this.batchSize = batchSize;
+        this.syncExecute = syncExecute;
         this.consumer = consumer;
         this.datum = new ArrayList<>(batchSize);
         this.taskThreadPool = TaskThreadPool.builder().build();
@@ -35,29 +42,24 @@ public final class AccumulateExecutor<T> {
     public void add(T data) {
         this.datum.add(data);
         if (this.datum.size() == this.batchSize) {
-            execute(this.datum, false);
+            execute(this.datum);
             this.datum = new ArrayList<>(this.batchSize);
         }
     }
 
-    private void execute(List<T> data, boolean waitAllTaskComplete) {
+    private void execute(List<T> data) {
         if (data.isEmpty()) {
             return;
         }
         this.taskThreadPool.execute(() -> consumer.accept(data));
-        close(waitAllTaskComplete);
     }
 
-    public void finish() {
-        execute(this.datum, false);
-    }
-
-    public void finishSync() {
-        execute(this.datum, true);
-    }
-
-    private void close(boolean shutdownUntilAllTaskComplete) {
-        this.taskThreadPool.shutdownUntilAllTaskComplete = shutdownUntilAllTaskComplete;
+    @Override
+    public void close() {
+        execute(this.datum);
+        if (this.syncExecute) {
+            this.taskThreadPool.shutdownUntilAllTaskComplete = true;
+        }
         this.taskThreadPool.shutdown();
     }
 
