1f2e2983aa8e107bc19e3f0cbe46a27344606ac2
DingHao <dh.hiekn@gmail.com>
2024-12-09 14:16:51 +0800

    Make McnAssert as abstract class
    (cherry picked from commit e0f31b98f16db9a2865656767127fe6b681e1a38)

--- mcn-boot-core/src/main/java/cn/hiboot/mcn/core/util/McnAssert.java
+++ mcn-boot-core/src/main/java/cn/hiboot/mcn/core/util/McnAssert.java
@@ -2,42 +2,66 @@ package cn.hiboot.mcn.core.util;
 
 import cn.hiboot.mcn.core.exception.ErrorMsg;
 
+import java.util.function.Supplier;
+
 /**
  * McnAssert
  *
  * @author DingHao
  * @since 2022/1/19 14:23
  */
-public interface McnAssert {
+public abstract class McnAssert {
 
-    static void notNull(Object object, String message) {
+    public static void notNull(Object object, String message) {
         if (object == null) {
             throw new IllegalArgumentException(message);
         }
     }
 
-    static void hasText(String text, String message) {
+    public static void notNull(Object object, Supplier<String> message) {
+        if (object == null) {
+            throw new IllegalArgumentException(nullSafeGet(message));
+        }
+    }
+
+    public static void hasText(String text, String message) {
         if (McnUtils.isNullOrEmpty(text)) {
             throw new IllegalArgumentException(message);
         }
     }
 
-    static void state(boolean state, String message) {
+    public static void hasText(String text, Supplier<String> message) {
+        if (McnUtils.isNullOrEmpty(text)) {
+            throw new IllegalArgumentException(nullSafeGet(message));
+        }
+    }
+
+    public static void state(boolean state, String message) {
         if (!state) {
             throw new IllegalArgumentException(message);
         }
     }
 
-    static void notNull(Object object, Integer errorCode) {
-        notNull(object, ErrorMsg.getErrorMsg(errorCode));
+    public static void state(boolean state, Supplier<String> message) {
+        if (!state) {
+            throw new IllegalArgumentException(nullSafeGet(message));
+        }
     }
 
-    static void hasText(String text, Integer errorCode) {
-        hasText(text, ErrorMsg.getErrorMsg(errorCode));
+    public static void notNull(Object object, Integer errorCode) {
+        notNull(object, () -> ErrorMsg.getErrorMsg(errorCode));
     }
 
-    static void state(boolean state, Integer errorCode) {
-        state(state, ErrorMsg.getErrorMsg(errorCode));
+    public static void hasText(String text, Integer errorCode) {
+        hasText(text, () -> ErrorMsg.getErrorMsg(errorCode));
+    }
+
+    public static void state(boolean state, Integer errorCode) {
+        state(state, () -> ErrorMsg.getErrorMsg(errorCode));
+    }
+
+    private static String nullSafeGet(Supplier<String> messageSupplier) {
+        return (messageSupplier != null ? messageSupplier.get() : null);
     }
 
 }
