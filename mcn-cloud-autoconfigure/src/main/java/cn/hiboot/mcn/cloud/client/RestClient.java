c9bd3e2cba547ca9089a66aa3f23ecb4be4b4f9e
DingHao <dh.hiekn@gmail.com>
2025-02-10 15:25:47 +0800

    Add rawClient method
    (cherry picked from commit 0b9ae38f38a4717cd585fdf9df912b90604486b7)

--- mcn-cloud-autoconfigure/src/main/java/cn/hiboot/mcn/cloud/client/RestClient.java
+++ mcn-cloud-autoconfigure/src/main/java/cn/hiboot/mcn/cloud/client/RestClient.java
@@ -8,7 +8,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.ResolvableType;
-import org.springframework.http.*;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Collections;
@@ -46,6 +51,10 @@ public class RestClient {
         this.restTemplate = restTemplate;
         this.wrapperClass = wrapperClass;
         this.extractData = extractData;
+    }
+
+    public static RestClient rawClient() {
+        return new RestClient(null, s -> s);
     }
 
     public <D> D getObject(String url, Class<D> resultClass) {
