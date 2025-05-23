26b5e086509f026338ff747492acde4b52ad512f
DingHao <dh.hiekn@gmail.com>
2025-05-23 13:54:34 +0800

    Add SchemaInfo

--- mcn-boot-autoconfigure/src/main/java/cn/hiboot/mcn/autoconfigure/jdbc/manage/DbType.java
+++ mcn-boot-autoconfigure/src/main/java/cn/hiboot/mcn/autoconfigure/jdbc/manage/DbType.java
@@ -1,45 +1,64 @@
 package cn.hiboot.mcn.autoconfigure.jdbc.manage;
 
-import org.springframework.boot.jdbc.DatabaseDriver;
-
 /**
  * DbType
  *
  * @author DingHao
  * @since 2025/5/15 14:11
  */
-enum DbType {
+public enum DbType {
 
+    mysql("com.mysql.cj.jdbc.Driver"),
+    oracle("oracle:thin:@", "oracle.jdbc.OracleDriver"),
+    postgresql("org.postgresql.Driver"),
+    sqlserver("com.microsoft.sqlserver.jdbc.SQLServerDriver"),
+    mariadb("org.mariadb.jdbc.Driver"),
     dm("dm.jdbc.driver.DmDriver"),
-    kingbase("com.kingbase8.Driver");
+    kingbase("kingbase8","com.kingbase8.Driver");
 
+    private final String platform;
     private final String driverClassName;
 
     DbType(String driverClassName) {
+        this(null, driverClassName);
+    }
+
+    DbType(String platform, String driverClassName) {
+        this.platform = platform;
         this.driverClassName = driverClassName;
     }
 
-    public static String fromTable(String dbType, String schema, String table) {
-        try {
-            valueOf(dbType);
-            return schema + "." + table;
-        } catch (Exception ignored) {
-
+    public String url(ConnectConfig connectConfig) {
+        String platform = this.platform == null ? this.name() : this.platform;
+        String colon = this == oracle ? "" : ":";
+        String url = "jdbc:" + platform + colon + "//" + connectConfig.getIp() + ":" + connectConfig.getPort() + "/";
+        if (this == dm) {
+            url += connectConfig.getSchema();
+        }else {
+            url += connectConfig.getCatalog();
         }
-        return table;
+        return url;
     }
 
-    public static String driverClassName(String type) {
-        try {
-            return valueOf(type).driverClassName;
-        } catch (Exception e) {
-            for (DatabaseDriver value : DatabaseDriver.values()) {
-                if (value.name().equalsIgnoreCase(type)) {
-                    return value.getDriverClassName();
-                }
-            }
-            throw new IllegalArgumentException(type + " is not supported");
+    public String getDriverClassName() {
+        return driverClassName;
+    }
+
+    public String pageSql(String sql, Integer skip, Integer limit) {
+        String pageSql;
+        if (this == oracle) {
+            pageSql = "select * from ( select rownum rn , t.* from (" + sql + ") t where rownum <= " + (limit + skip) + ") tt where tt.rn > " + skip;
+        } else {
+            pageSql = sql + " limit :skip,:pageSize";
         }
+        return pageSql;
+    }
+
+    public String sqlQuote() {
+        if (this == mysql || this == mariadb) {
+            return "`";
+        }
+        return "\"";
     }
 
 }
