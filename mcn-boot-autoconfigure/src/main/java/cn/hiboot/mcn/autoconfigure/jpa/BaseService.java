b8b86d17580929c5df1af3d652d356c045b76c8e
DingHao <dh.hiekn@gmail.com>
2025-07-28 10:55:42 +0800

    Add CrudService

--- mcn-boot-autoconfigure/src/main/java/cn/hiboot/mcn/autoconfigure/jpa/BaseService.java
+++ mcn-boot-autoconfigure/src/main/java/cn/hiboot/mcn/autoconfigure/jpa/BaseService.java
@@ -1,5 +1,6 @@
 package cn.hiboot.mcn.autoconfigure.jpa;
 
+import cn.hiboot.mcn.core.model.CrudService;
 import cn.hiboot.mcn.core.model.base.FieldSort;
 import cn.hiboot.mcn.core.model.base.PageSort;
 import cn.hiboot.mcn.core.model.result.RestResp;
@@ -17,26 +18,29 @@ import java.util.List;
  * @author DingHao
  * @since 2021/10/11 13:39
  */
-public interface BaseService<T, PK, R extends BaseRepository<T, PK>> {
+public interface BaseService<T, PK, R extends BaseRepository<T, PK>> extends CrudService<T, PK> {
 
     default R getRepository() {
         return JpaUtils.getRepository(this.getClass());
     }
 
-    default T save(T data) {
+    @Override
+    default T save(T data){
         beforeSave(data);
-        return getRepository().save(data);
+        return getRepository().save( data );
     }
 
-    default List<T> saveAll(List<T> data) {
-        return getRepository().saveAll(data);
+    @Override
+    default List<T> saveAll(List<T> data){
+        return getRepository().saveAll( data );
     }
 
-    default void beforeSave(T data) {
+    default void beforeSave(T data){
 
     }
 
-    default void deleteById(PK id) {
+    @Override
+    default void deleteById(PK id){
         try {
             getRepository().deleteById(id);
         } catch (Exception ignored) {
@@ -44,7 +48,8 @@ public interface BaseService<T, PK, R extends BaseRepository<T, PK>> {
         }
     }
 
-    default void deleteByIds(Collection<PK> ids) {
+    @Override
+    default void deleteByIds(Collection<PK> ids){
         try {
             getRepository().deleteAllById(ids);
         } catch (Exception ignored) {
@@ -52,60 +57,73 @@ public interface BaseService<T, PK, R extends BaseRepository<T, PK>> {
         }
     }
 
-    default T getById(PK id) {
+    @Override
+    default T getById(PK id){
         return getRepository().findById(id).orElse(null);
     }
 
-    default List<T> getByIds(Collection<PK> ids) {
+    @Override
+    default List<T> getByIds(Collection<PK> ids){
         return getRepository().findAllById(ids);
     }
 
-    default T getOne(T t) {
+    default T getOne(T t){
         return getRepository().findOne(Example.of(t)).orElse(null);
     }
 
-    default List<T> list(T t) {
+    @Override
+    default List<T> list(T t){
         return getRepository().findAll(Example.of(t));
     }
 
-    default List<T> list(T t, List<FieldSort> sort) {
-        return list(new ExampleSpecification<>(t), sort);
+    @Override
+    default List<T> list(T t, List<FieldSort> sort){
+        return getRepository().findAll(Example.of(t),JpaUtils.jpaSort(sort));
     }
 
-    default RestResp<List<T>> page(T t, PageSort pageSort) {
-        return page(new ExampleSpecification<>(t), pageSort);
+    default RestResp<List<T>> page(T t, PageSort pageSort){
+        PageRequest pageRequest = PageRequest.of(pageSort.getPageIndex(),pageSort.getPageSize(),JpaUtils.jpaSort(pageSort.getSort()));
+        Page<T> page;
+        if(t == null){
+            page = getRepository().findAll(pageRequest);
+        }else {
+            page = getRepository().findAll(Example.of(t),pageRequest);
+        }
+        return RestResp.ok(page.getContent(),page.getTotalElements());
     }
 
-    default T getOne(Specification<T> s) {
+    default T getOne(Specification<T> s){
         return getRepository().findOne(s).orElse(null);
     }
 
-    default List<T> list(Specification<T> s) {
+    default List<T> list(Specification<T> s){
         return getRepository().findAll(s);
     }
 
-    default List<T> list(Specification<T> s, List<FieldSort> sort) {
-        return getRepository().findAll(s, JpaUtils.jpaSort(sort));
+    default List<T> list(Specification<T> s, List<FieldSort> sort){
+        return getRepository().findAll(s,JpaUtils.jpaSort(sort));
     }
 
-    default RestResp<List<T>> page(Specification<T> s, PageSort pageSort) {
-        PageRequest pageRequest = PageRequest.of(pageSort.getPageIndex(), pageSort.getPageSize(), JpaUtils.jpaSort(pageSort.getSort()));
-        Page<T> page = getRepository().findAll(s, pageRequest);
-        return RestResp.ok(page.getContent(), page.getTotalElements());
+    default RestResp<List<T>> page(Specification<T> s, PageSort pageSort){
+        PageRequest pageRequest = PageRequest.of(pageSort.getPageIndex(),pageSort.getPageSize(),JpaUtils.jpaSort(pageSort.getSort()));
+        Page<T> page = getRepository().findAll(s,pageRequest);
+        return RestResp.ok(page.getContent(),page.getTotalElements());
     }
 
-    default void updateById(PK id, T data) {
+    @Override
+    default void updateById(PK id, T data){
         getRepository().findById(id).ifPresent(d -> {
-            beforeUpdate(d, data);
-            JpaUtils.copyTo(data, d);
+            beforeUpdate(d,data);
+            JpaUtils.copyTo(data,d);
             getRepository().save(d);
         });
     }
 
-    default void beforeUpdate(T oldData, T newData) {
+    default void beforeUpdate(T oldData, T newData){
 
     }
 
+    @Override
     default long count(T t) {
         return getRepository().count(Example.of(t));
     }
