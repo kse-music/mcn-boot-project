cd6b7a05badc538d33ac353ee1e97889c56f3e05
DingHao <dh.hiekn@gmail.com>
2025-11-11 11:22:01 +0800

    Supports implement DbTypeManager

--- mcn-boot-autoconfigure/src/main/java/cn/hiboot/mcn/autoconfigure/jdbc/manage/DbTypeManager.java
+++ mcn-boot-autoconfigure/src/main/java/cn/hiboot/mcn/autoconfigure/jdbc/manage/DbTypeManager.java
@@ -1,10 +1,6 @@
 package cn.hiboot.mcn.autoconfigure.jdbc.manage;
 
 import java.util.Collection;
-import java.util.Collections;
-import java.util.Map;
-import java.util.ServiceLoader;
-import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * DbTypeManager
@@ -12,41 +8,16 @@ import java.util.concurrent.ConcurrentHashMap;
  * @author DingHao
  * @since 2025/11/11 9:44
  */
-public abstract class DbTypeManager {
+public interface DbTypeManager {
 
-    private static final Map<String, DbTypeProvider> REGISTRY = new ConcurrentHashMap<>();
+    void register(DbTypeProvider... providers);
 
-    static {
-        registerDefault();
-        ServiceLoader.load(DbTypeProvider.class).forEach(DbTypeManager::register);
-    }
+    DbTypeProvider get(String name);
 
-    private DbTypeManager() {
+    Collection<DbTypeProvider> all();
 
-    }
-
-    private static void registerDefault() {
-        register(new DbTypeModel("mysql", "com.mysql.cj.jdbc.Driver"),
-                new DbTypeModel("postgresql", "org.postgresql.Driver"),
-                new DbTypeModel("sqlserver", "com.microsoft.sqlserver.jdbc.SQLServerDriver"),
-                new DbTypeModel("oracle", "oracle.jdbc.OracleDriver", "oracle:thin:@"),
-                new DbTypeModel("mariadb", "org.mariadb.jdbc.Driver"),
-                new DbTypeModel("dm", "dm.jdbc.driver.DmDriver"),
-                new DbTypeModel("kingbase", "com.kingbase8.Driver", "kingbase8"));
-    }
-
-    public static void register(DbTypeProvider... providers) {
-        for (DbTypeProvider provider : providers) {
-            REGISTRY.put(provider.name().toLowerCase(), provider);
-        }
-    }
-
-    public static DbTypeProvider get(String name) {
-        return REGISTRY.get(name.toLowerCase());
-    }
-
-    public static Collection<DbTypeProvider> all() {
-        return Collections.unmodifiableCollection(REGISTRY.values());
+    static DbTypeManager defaults() {
+        return DefaultDbTypeManager.defaults;
     }
 
 }
