886beb1d63ea30db95f93bf3f7ad79c83aa4fe6e
DingHao <dh.hiekn@gmail.com>
2025-11-11 11:22:01 +0800

    Supports implement DbTypeManager
    (cherry picked from commit 4f948d6aeb64181bddc283efe3ba3ccaedc78196)

--- mcn-boot-autoconfigure/src/main/java/cn/hiboot/mcn/autoconfigure/jdbc/manage/DbTypeManager.java
+++ mcn-boot-autoconfigure/src/main/java/cn/hiboot/mcn/autoconfigure/jdbc/manage/DbTypeManager.java
@@ -1,23 +1,49 @@
 package cn.hiboot.mcn.autoconfigure.jdbc.manage;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.Map;
+import java.util.ServiceLoader;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
- * DbTypeManager
+ * DefaultDbTypeManager
  *
  * @author DingHao
  * @since 2025/11/11 9:44
  */
-public interface DbTypeManager {
+public abstract class DbTypeManager {
 
-    void register(DbTypeProvider... providers);
+    private static final Map<String, DbTypeProvider> REGISTRY = new ConcurrentHashMap<>();
 
-    DbTypeProvider get(String name);
+    static {
+        registerDefault();
+        ServiceLoader.load(DbTypeProvider.class).forEach(DbTypeManager::register);
+    }
 
-    Collection<DbTypeProvider> all();
+    private static void registerDefault() {
+        register(new DbTypeModel("mysql", "com.mysql.cj.jdbc.Driver"),
+                new DbTypeModel("postgresql", "org.postgresql.Driver"),
+                new DbTypeModel("sqlserver", "com.microsoft.sqlserver.jdbc.SQLServerDriver"),
+                new DbTypeModel("oracle", "oracle.jdbc.OracleDriver", "oracle:thin:@"),
+                new DbTypeModel("mariadb", "org.mariadb.jdbc.Driver"),
+                new DbTypeModel("dm", "dm.jdbc.driver.DmDriver"),
+                new DbTypeModel("kingbase", "com.kingbase8.Driver", "kingbase8"));
+    }
 
-    static DbTypeManager defaults() {
-        return DefaultDbTypeManager.defaults;
+    public static void register(DbTypeProvider... providers) {
+        for (DbTypeProvider provider : providers) {
+            REGISTRY.put(provider.name().toLowerCase(), provider);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T extends DbTypeProvider> T get(String name) {
+        return (T) REGISTRY.get(name.toLowerCase());
+    }
+
+    public static Collection<DbTypeProvider> all() {
+        return Collections.unmodifiableCollection(REGISTRY.values());
     }
 
 }
