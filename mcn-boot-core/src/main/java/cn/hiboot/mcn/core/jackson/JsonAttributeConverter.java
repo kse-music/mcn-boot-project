bfca61e270edc3e60267808a6f520afb24880a52
kse-music <dh.hiekn@gmail.com>
2025-12-17 21:37:13 +0800

    Add JsonAttributeConverter

---
+++ mcn-boot-core/src/main/java/cn/hiboot/mcn/core/jackson/JsonAttributeConverter.java
@@ -0,0 +1,39 @@
+package cn.hiboot.mcn.core.jackson;
+
+import cn.hiboot.mcn.core.util.JacksonUtils;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+/**
+ * JsonAttributeConverter
+ *
+ * @author DingHao
+ * @since 2025/2/24 14:19
+ */
+@Converter
+public abstract class JsonAttributeConverter<T> implements AttributeConverter<T, String> {
+
+    private final JavaType javaType;
+
+    public JsonAttributeConverter(JavaType javaType) {
+        this.javaType = javaType;
+    }
+
+    @Override
+    public String convertToDatabaseColumn(T t) {
+        return JacksonUtils.toJson(t);
+    }
+
+    @Override
+    public T convertToEntityAttribute(String str) {
+        return JacksonUtils.toBean(str, javaType);
+    }
+
+    protected static TypeFactory getTypeFactory() {
+        return JacksonUtils.getTypeFactory();
+    }
+
+}
